3e53271fe42fda6893fa2a0e45c2b719df918621
Maksims Mihejevs <max@playcanvas.com>
2015-01-29 19:18:54 +0000

    color picker wip

--- editor/public/js/editor/pickers/picker-color.js
+++ editor/public/js/editor/pickers/picker-color.js
@@ -11,6 +11,33 @@ editor.once('load', function() {
     pickRect.classList.add('pick-rect');
     overlay.append(pickRect);
 
+    var pickRectMouseMove = function(evt) {
+        var rect = pickRect.getBoundingClientRect();
+        var x = Math.max(0, Math.min(pickRect.clientWidth, Math.floor(evt.clientX - rect.left)));
+        var y = Math.max(0, Math.min(pickRect.clientHeight, Math.floor(evt.clientY - rect.top)));
+
+        pickRectHandle.style.left = x + 'px';
+        pickRectHandle.style.top = y + 'px';
+
+        var s = x / pickRect.clientWidth;
+        var l = 1.0 - (y / pickRect.clientHeight);
+
+        console.log(s, l)
+    };
+
+    var pickRectMouseUp = function() {
+        window.removeEventListener('mousemove', pickRectMouseMove, false);
+        window.removeEventListener('mouseup', pickRectMouseUp, false);
+    };
+
+    pickRect.addEventListener('mousedown', function(evt) {
+        window.addEventListener('mousemove', pickRectMouseMove, false);
+        window.addEventListener('mouseup', pickRectMouseUp, false);
+        evt.stopPropagation();
+        evt.preventDefault();
+    });
+
+
     // white
     var pickRectWhite = document.createElement('div');
     pickRectWhite.classList.add('white');
@@ -20,6 +47,11 @@ editor.once('load', function() {
     var pickRectBlack = document.createElement('div');
     pickRectBlack.classList.add('black');
     pickRect.appendChild(pickRectBlack);
+
+    // handle
+    var pickRectHandle = document.createElement('div');
+    pickRectHandle.classList.add('handle');
+    pickRect.appendChild(pickRectHandle);
 
 
     // hue (rainbow) picker
