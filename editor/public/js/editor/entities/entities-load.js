bcf5e53621dd9a5e779f58c78900396e1978b318
Maksims Mihejevs <max@playcanvas.com>
2015-02-19 14:31:27 +0000

    wip

--- editor/public/js/editor/entities/entities-load.js
+++ editor/public/js/editor/entities/entities-load.js
@@ -18,10 +18,6 @@ editor.on('load', function() {
 
         // list
         for(var key in data.entities) {
-            // components.script.scripts > ObserverList
-            // if (data.entities[key].components.script && data.entities[key].components.script.scripts.length === 0)
-            //     data.entities[key].components.script.scripts = ObserverList
-
             editor.call('entities:add', new Observer(data.entities[key]));
 
             p.progress = (++i / total) * .8 + .1;
