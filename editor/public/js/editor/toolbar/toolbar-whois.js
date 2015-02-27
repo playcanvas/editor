7b4b150008fc527497f608e1a90cf2f899592921
Maksims Mihejevs <max@playcanvas.com>
2015-02-27 19:29:16 +0000

    no whois

--- editor/public/js/editor/toolbar/toolbar-whois.js
+++ editor/public/js/editor/toolbar/toolbar-whois.js
@@ -2,12 +2,4 @@ editor.once('load', function() {
     'use strict';
 
     var viewport = editor.call('layout.viewport');
-
-    var userA = document.createElement('div');
-    userA.id = 'user-a';
-    viewport.append(userA);
-
-    var userB = document.createElement('div');
-    userB.id = 'user-b';
-    viewport.append(userB);
 });
