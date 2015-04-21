571662ae04007866f99005012dfe8cd7d6fc28a1
Maksims Mihejevs <max@playcanvas.com>
2015-04-21 10:08:11 +0100

    make array extensions non enumerable

--- editor/public/js/array.js
+++ editor/public/js/array.js
@@ -1,30 +1,35 @@
-Array.prototype.equals = function(array) {
-    if (! array)
-        return false;
+Object.defineProperty(Array.prototype, 'equals', {
+    enumerable: false,
+    value: function(array) {
+        if (! array)
+            return false;
 
-    if (this.length !== array.length)
-        return false;
+        if (this.length !== array.length)
+            return false;
 
-    for (var i = 0, l = this.length; i < l; i++) {
-        if (this[i] instanceof Array && array[i] instanceof Array) {
-            if (! this[i].equals(array[i]))
+        for (var i = 0, l = this.length; i < l; i++) {
+            if (this[i] instanceof Array && array[i] instanceof Array) {
+                if (! this[i].equals(array[i]))
+                    return false;
+            } else if (this[i] !== array[i]) {
                 return false;
-        } else if (this[i] !== array[i]) {
-            return false;
+            }
         }
+        return true;
     }
-    return true;
-};
+});
 
+Object.defineProperty(Array.prototype, 'match', {
+    enumerable: false,
+    value: function(pattern) {
+        if (this.length !== pattern.length)
+            return;
 
-Array.prototype.match = function(pattern) {
-    if (this.length !== pattern.length)
-        return;
+        for(var i = 0, l = this.length; i < l; i++) {
+            if (pattern[i] !== '*' && pattern[i] !== this[i])
+                return false;
+        }
 
-    for(var i = 0, l = this.length; i < l; i++) {
-        if (pattern[i] !== '*' && pattern[i] !== this[i])
-            return false;
+        return true;
     }
-
-    return true;
-};
+});
