e7e9eba7482c8f264fc3cde70f58f1db72037bc6
Dave Evans <dave@playcanvas.com>
2015-08-04 14:13:46 +0100

    Update code to same path applying scene settings on load as in published app

--- editor/public/js/launch/viewport.js
+++ editor/public/js/launch/viewport.js
@@ -57,7 +57,7 @@ app.once('load', function() {
                     application.root.addChild(application.scene.root);
 
                     // update scene settings now that scene is loaded
-                    application.updateSceneSettings(sceneSettings);
+                    application.applySceneSettings(sceneSettings);
 
                     // clear stored loading data
                     sceneData = null;
