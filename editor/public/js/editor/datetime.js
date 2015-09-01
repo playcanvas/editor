677cfa0cc824a0112b90ee2a04e39f1bcebd0994
Vaios Kalpias Ilias <vaios@playcanvas.com>
2015-09-01 12:55:24 +0300

    Return local date without moment.js

--- editor/public/js/editor/datetime.js
+++ editor/public/js/editor/datetime.js
@@ -3,16 +3,6 @@ editor.once('load', function () {
 
     // convert passed time to a local time with moment.js
     editor.method('datetime:convert', function (date) {
-        var mom = moment.utc(date).local();
-        var now = new moment();
-        if (now.diff(mom, 'hour') <= 24) {
-            if (now.diff(mom, 'second') < 0) {
-                return 'just now';
-            } else {
-                return mom.fromNow();
-            }
-        } else {
-            return mom.format('MMM D [\']YY H:mm');
-        }
+        return new Date(date).toLocaleString();
     });
 });
