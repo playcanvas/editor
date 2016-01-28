9b03f800d2ccb0a1d99bd65954587fef28af352c
Maksims Mihejevs <max@playcanvas.com>
2016-01-28 12:56:58 +0000

    ..

--- editor/public/js/editor/entities/entities-migrations.js
+++ editor/public/js/editor/entities/entities-migrations.js
@@ -9,17 +9,19 @@ editor.once('load', function() {
         // light
         if (entity.has('components.light')) {
             // affectDynamic
-            if (! entity.has('components.light.affectDynamic')) {
+            if (! entity.has('components.light.affectDynamic'))
                 entity.set('components.light.affectDynamic', true);
-            }
         }
 
         // model
         if(entity.has('components.model')) {
+            // castShadowsLightmap
+            if (! entity.has('components.model.castShadowsLightmap'))
+                entity.set('components.model.castShadowsLightmap', true);
+
             // lightmapSizeMultiplier
-            if (! entity.has('components.model.lightmapSizeMultiplier')) {
+            if (! entity.has('components.model.lightmapSizeMultiplier'))
                 entity.set('components.model.lightmapSizeMultiplier', 1.0);
-            }
         }
 
         entity.history.enabled = true;
