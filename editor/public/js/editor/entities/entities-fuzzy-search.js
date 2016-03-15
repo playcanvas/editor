5b6cfa3693a0e047fefd005479e1d1c4e11b5f7f
Makism Mihejevs <core@moka.co>
2016-03-15 12:42:24 +0000

    entities fuzzy-search multi-token search

--- editor/public/js/editor/entities/entities-fuzzy-search.js
+++ editor/public/js/editor/entities/entities-fuzzy-search.js
@@ -1,7 +1,6 @@
 editor.once('load', function() {
     'use strict';
 
-    var MIN_CHARS = 3;
     var LIMIT_RESULT = 16;
     var CONTAINS_CHARS_TOLERANCE = 0.5;
     var EDITS_DISTANCE_TOLERANCE = 0.5;
@@ -59,10 +58,19 @@ editor.once('load', function() {
 
     var stringTokenizer = function(name) {
         var tokens = [ ];
-        var parts = name.split(/(\s|\-|_)/g);
 
+        // camelCase
+        // upperCASE123
+        var string = name.replace(/([^A-Z])([A-Z][^A-Z])/g, '$1 $2').replace(/([A-Z0-9]{2,})/g, ' $1');
+
+        // space notation
+        // dash-notation
+        // underscore_notation
+        var parts = string.split(/(\s|\-|_)/g);
+
+        // filter valid tokens
         for(var i = 0; i < parts.length; i++) {
-            parts[i] = parts[i].trim();
+            parts[i] = parts[i].toLowerCase().trim();
             if (parts[i] && parts[i] !== '-' && parts[i] !== '_')
                 tokens.push(parts[i]);
         }
@@ -74,89 +82,123 @@ editor.once('load', function() {
     editor.method('string-edit-distance', stringsEditDistance);
 
 
-    editor.method('entities:fuzzy-search', function(query, limit) {
-        var search = (query || '').toLowerCase().trim();
+    editor.method('string-tokenizer', stringTokenizer);
 
-        if (! search || search.length < MIN_CHARS)
+
+    editor.method('entities:fuzzy-search', function(query, limit) {
+        var search = (query || '').trim();
+
+        if (! search)
             return [ ];
 
-        var entities = editor.call('entities:list');
+        var searchTokens = stringTokenizer(search);
+        if (! searchTokens.length)
+            return [ ];
+
+        var items = editor.call('entities:list');
         var result = [ ];
 
-        for(var i = 0; i < entities.length; i++) {
-            var name = entities[i].get('name').toLowerCase().trim();
+        for(var i = 0; i < items.length; i++) {
+            var name = items[i].get('name');
 
-            // direct hit
-            if (name === search || name.indexOf(search) === 0) {
-                result.push([ entities[i], name, 0, 0 ]);
-                continue;
-            }
-
-            // check if name contains enough of search characters
-            var contains = charsContains(search, name);
-            if (contains / search.length < CONTAINS_CHARS_TOLERANCE)
-                continue;
-
-            // tokenize name
-            // this splits name to separate strings to be compared
-            // using this regexp: (\s|\-|_)
-            var tokens = stringTokenizer(name);
-            var closest = Infinity;
-            var sub = Infinity;
-
-            // for each token
-            for(var t = 0; t < tokens.length; t++) {
-                // equal match
-                if (tokens[t] === search) {
-                    closest = 0;
-                    sub = t;
-                    break;
-                }
-
-                // calculate edit distance
-                var edits = stringsEditDistance(search, tokens[t]);
-
-                if (sub === Infinity && tokens[t].indexOf(search) !== -1) { // is a substring
-                    sub = t;
-                    closest = edits;
-                    break;
-                } else if (sub === Infinity && edits < closest) { // new edits candidate
-                    // make sure number of edits is below edits tolerance
-                    if (edits / Math.max(search.length, tokens[t].length) < EDITS_DISTANCE_TOLERANCE)
-                        closest = edits;
-                }
-
-                if (closest === 0)
-                    break;
-            }
-
-            if (closest === Infinity)
-                continue;
-
-            // add to results
-            result.push([ entities[i], name, closest, sub ]);
+            items[i] = {
+                item: items[i],
+                name: name.toLowerCase().trim(),
+                tokens: stringTokenizer(name),
+                edits: Infinity,
+                sub: Infinity
+            };
         }
 
+        var searchItems = function(items, search) {
+            var results = [ ];
+
+            for(var i = 0; i < items.length; i++) {
+                var item = items[i];
+
+                // direct hit
+                if (item.name === search || item.name.indexOf(search) === 0) {
+                    results.push(item);
+
+                    if (item.edits === Infinity)
+                        item.edits = 0;
+
+                    if (item.sub === Infinity)
+                        item.sub = 0;
+
+                    continue;
+                }
+
+                // check if name contains enough of search characters
+                var contains = charsContains(search, item.name);
+                if (contains / search.length < CONTAINS_CHARS_TOLERANCE)
+                    continue;
+
+                var editsCandidate = Infinity;
+                var subCandidate = Infinity;
+
+                // for each token
+                for(var t = 0; t < item.tokens.length; t++) {
+                    // direct token match
+                    if (item.tokens[t] === search) {
+                        editsCandidate = 0;
+                        subCandidate = t;
+                        break;
+                    }
+
+                    var edits = stringsEditDistance(search, item.tokens[t]);
+
+                    if ((subCandidate === Infinity || edits < editsCandidate) && item.tokens[t].indexOf(search) !== -1) {
+                        // search is a substring of a token
+                        subCandidate = t;
+                        editsCandidate = edits;
+                        continue;
+                    } else if (subCandidate === Infinity && edits < editsCandidate) {
+                        // new edits candidate, not a substring of a token
+                        if ((edits / Math.max(search.length, item.tokens[t].length)) < EDITS_DISTANCE_TOLERANCE) {
+                            // check if edits tolerance is satisfied
+                            editsCandidate = edits;
+                        }
+                    }
+                }
+
+                // no match candidate
+                if (editsCandidate === Infinity)
+                    continue;
+
+                // add new result
+                results.push(item);
+                item.edits = item.edits === Infinity ? editsCandidate : item.edits + editsCandidate;
+                item.sub = item.sub === Infinity ? subCandidate : item.sub + subCandidate;
+            }
+
+            return results;
+        };
+
+        // search each token
+        for(var i = 0; i < searchTokens.length; i++)
+            items = searchItems(items, searchTokens[i]);
+
         // sort result first by substring? then by edits number
-        result.sort(function(a, b) {
-            if (a[3] !== b[3]) {
-                return a[3] - b[3];
-            } else if (a[2] !== b[2]) {
-                return a[2] - b[2];
+        items.sort(function(a, b) {
+            if (a.sub !== b.sub) {
+                return a.sub - b.sub;
+            } else if (a.edits !== b.edits) {
+                return a.edits - b.edits;
             } else {
-                return a[1].length - b[1].length;
+                return a.name.length - b.name.length;
             }
         });
 
         // return only entities without match information
-        for(var i = 0; i < result.length; i++)
-            result[i] = result[i][0];
+        for(var i = 0; i < items.length; i++)
+            items[i] = items[i].item;
 
         // limit number of results
-        if (result.length > (limit || LIMIT_RESULT))
-            result = result.slice(0, limit || LIMIT_RESULT);
+        if (items.length > (limit || LIMIT_RESULT))
+            items = items.slice(0, limit || LIMIT_RESULT);
 
         // return results
-        return result;
+        return items;
     });
 });
