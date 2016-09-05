3a49bf0f90d45e94daaeff5753b605e7dbc8028e
Vaios Kalpias Ilias <vaios@playcanvas.com>
2016-09-05 10:19:23 +0300

    Code editor now allows editing even when offline - on reconnect all local and remote changes are synced up

--- editor/public/js/code_editor/editor-codemirror.js
+++ editor/public/js/code_editor/editor-codemirror.js
@@ -227,8 +227,14 @@ editor.once('load', function () {
     });
 
     editor.on('editor:reloadScript', function (data) {
-        var isDirty = editor.call('editor:isDirty');
+        // if the reloaded data are different
+        // than the current editor value then reset the contents
+        // of the editor - that can happen if a change has been rolled back
+        // by sharejs for example
+        if (codeMirror.getValue() === data)
+            return;
 
+        var isDirty = editor.call('editor:isDirty');
         isLoading = true;
         code = data;
         codeMirror.setValue(code);
@@ -237,6 +243,7 @@ editor.once('load', function () {
             codeMirror.markClean();
 
         isLoading = false;
+
     });
 
     // emit change
