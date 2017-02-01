674602fd7c0eaab25399d9d65dcb60ff66aa1397
Vaios Kalpias Ilias <vaios@playcanvas.com>
2017-02-01 19:57:11 +0200

    Add Edit->Text menu group

---
+++ editor/public/js/code-editor-v2/menu-panel/menu-edit-text.js
@@ -0,0 +1,198 @@
+editor.once('load', function () {
+    'use strict';
+
+    var menu = editor.call('menu:edit');
+    var cm = editor.call('editor:codemirror');
+    var codePanel = editor.call('layout.code');
+
+    var mac = navigator.userAgent.indexOf('Mac OS X') !== -1;
+
+    var canEditLine = function () {
+        return editor.call('documents:getFocused') && !cm.isReadOnly();
+    };
+
+    var group = menu.createItem('text', {
+        title: 'Text'
+    });
+    group.class.add('text');
+    menu.append(group);
+
+    // Insert line before
+    var item = menu.createItem('insert-line-before', {
+        title: 'Insert Line Before',
+        filter: function () {
+            return editor.call('editor:command:can:insertLineBefore');
+        },
+        select: function () {
+            return editor.call('editor:command:insertLineBefore');
+        }
+    });
+    item.class.add('noBorder');
+    editor.call('menu:item:setShortcut', item, 'Shift+' + editor.call('hotkey:ctrl:string') + '+Enter');
+    group.append(item);
+
+    // hotkeys
+    editor.call('hotkey:register', 'insert-line-before', {
+        key: 'enter',
+        shift: true,
+        ctrl: true,
+        callback: function (e) {
+            if (! codePanel.element.contains(e.target))
+                editor.call('editor:command:insertLineBefore');
+        }
+    });
+
+    editor.method('editor:command:can:insertLineBefore', canEditLine);
+
+    editor.method('editor:command:insertLineBefore', function () {
+        if (! editor.call('editor:command:can:insertLineBefore')) return;
+        cm.execCommand('insertLineBefore');
+        cm.focus();
+    });
+
+    // Insert line after
+    var item = menu.createItem('insert-line-after', {
+        title: 'Insert Line After',
+        filter: function () {
+            return editor.call('editor:command:can:insertLineAfter');
+        },
+        select: function () {
+            return editor.call('editor:command:insertLineAfter');
+        }
+    });
+    editor.call('menu:item:setShortcut', item, editor.call('hotkey:ctrl:string') + '+Enter');
+    group.append(item);
+
+    // hotkeys
+    editor.call('hotkey:register', 'insert-line-after', {
+        key: 'enter',
+        ctrl: true,
+        callback: function (e) {
+            if (! codePanel.element.contains(e.target))
+                editor.call('editor:command:insertLineAfter');
+        }
+    });
+
+    editor.method('editor:command:can:insertLineAfter', canEditLine);
+
+    editor.method('editor:command:insertLineAfter', function () {
+        if (! editor.call('editor:command:can:insertLineAfter')) return;
+        cm.execCommand('insertLineAfter');
+        cm.focus();
+    });
+
+    // Delete word backward
+    var item = menu.createItem('delete-word-backward', {
+        title: 'Delete Word Backward',
+        filter: function () {
+            return editor.call('editor:command:can:deleteWordBackward');
+        },
+        select: function () {
+            return editor.call('editor:command:deleteWordBackward');
+        }
+    });
+    item.class.add('noBorder');
+    editor.call('menu:item:setShortcut', item, mac ? 'Alt+Backspace' : 'Ctrl+Backspace');
+    group.append(item);
+
+    // hotkeys
+    editor.call('hotkey:register', 'delete-word-backward', {
+        key: 'backspace',
+        ctrl: !mac,
+        alt: mac,
+        callback: function (e) {
+            if (! codePanel.element.contains(e.target))
+                editor.call('editor:command:deleteWordBackward');
+        }
+    });
+
+    editor.method('editor:command:can:deleteWordBackward', canEditLine);
+
+    editor.method('editor:command:deleteWordBackward', function () {
+        if (! editor.call('editor:command:can:deleteWordBackward')) return;
+        cm.execCommand('delGroupBefore');
+        cm.focus();
+    });
+
+    // Delete word forward
+    var item = menu.createItem('delete-word-forward', {
+        title: 'Delete Word Forward',
+        filter: function () {
+            return editor.call('editor:command:can:deleteWordForward');
+        },
+        select: function () {
+            return editor.call('editor:command:deleteWordForward');
+        }
+    });
+    item.class.add('noBorder');
+    editor.call('menu:item:setShortcut', item, mac ? 'Ctrl+Alt+Backspace' : 'Ctrl+Delete');
+    group.append(item);
+
+    // hotkeys
+    editor.call('hotkey:register', 'delete-word-forward', {
+        key: mac ? 'backspace' : 'delete',
+        ctrl: true,
+        alt: mac,
+        callback: function (e) {
+            if (! codePanel.element.contains(e.target))
+                editor.call('editor:command:deleteWordForward');
+        }
+    });
+
+    editor.method('editor:command:can:deleteWordForward', canEditLine);
+
+    editor.method('editor:command:deleteWordForward', function () {
+        if (! editor.call('editor:command:can:deleteWordForward')) return;
+        cm.execCommand('delGroupAfter');
+        cm.focus();
+    });
+
+    // Delete Beginning
+    var item = menu.createItem('delete-beginning', {
+        title: 'Delete To Beginning',
+        filter: function () {
+            return editor.call('editor:command:can:deleteBeginning');
+        },
+        select: function () {
+            return editor.call('editor:command:deleteBeginning');
+        }
+    });
+    item.class.add('noBorder');
+    editor.call('menu:item:setShortcut', item, mac ? 'Cmd+K Cmd+Backspace' : 'Ctrl+K Ctrl+Backspace');
+    group.append(item);
+
+    // no hotkey for that if not focused
+    // because we currently don't support combinations
+
+    editor.method('editor:command:can:deleteBeginning', canEditLine);
+
+    editor.method('editor:command:deleteBeginning', function () {
+        if (! editor.call('editor:command:can:deleteBeginning')) return;
+        cm.execCommand('delLineLeft');
+        cm.focus();
+    });
+
+    // Delete End
+    var item = menu.createItem('delete-end', {
+        title: 'Delete To End',
+        filter: function () {
+            return editor.call('editor:command:can:deleteEnd');
+        },
+        select: function () {
+            return editor.call('editor:command:deleteEnd');
+        }
+    });
+    editor.call('menu:item:setShortcut', item, mac ? 'Cmd+K Cmd+K' : 'Ctrl+K Ctrl+K');
+    group.append(item);
+
+    // no hotkey for that if not focused
+    // because we currently don't support combinations
+
+    editor.method('editor:command:can:deleteEnd', canEditLine);
+
+    editor.method('editor:command:deleteEnd', function () {
+        if (! editor.call('editor:command:can:deleteEnd')) return;
+        cm.execCommand('delLineRight');
+        cm.focus();
+    });
+});
