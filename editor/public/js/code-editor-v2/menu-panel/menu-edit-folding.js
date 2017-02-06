5e390fdaab5bc485216eddee3915afcbf1b86589
Vaios Kalpias Ilias <vaios@playcanvas.com>
2017-02-06 20:02:24 +0200

    Fix typo

--- editor/public/js/code-editor-v2/menu-panel/menu-edit-folding.js
+++ editor/public/js/code-editor-v2/menu-panel/menu-edit-folding.js
@@ -12,7 +12,7 @@ editor.once('load', function () {
     menu.append(group);
 
     var canFold = function () {
-        return !!editor.call('documents:hasFocused');
+        return !!editor.call('documents:getFocused');
     };
 
     // Fold
