b6ada04e45f914b3317524663dfbf1ef33a35579
Vaios Kalpias Ilias <vaios@playcanvas.com>
2017-02-07 19:26:09 +0200

    Prettier who is online with colors

---
+++ editor/public/js/code-editor-v2/users/users.js
@@ -0,0 +1,34 @@
+editor.once('load', function() {
+    'use strict';
+
+    var users = { };
+    var userRequests = { };
+
+    // Loads a user from the server
+    editor.method('users:loadOne', function (id, callback) {
+        if (users[id])
+            return callback(users[id]);
+
+        if (userRequests[id])
+            return userRequests[id].push(callback);
+
+        userRequests[id] = [ callback ];
+
+        Ajax({
+            url: '{{url.api}}/users/' + id,
+            auth: true
+        })
+        .on('load', function (status, data) {
+            users[id] = data;
+
+            for(var i = 0; i < userRequests[id].length; i++)
+                userRequests[id][i](data);
+
+            delete userRequests[id];
+        });
+    });
+
+    editor.method('users:get', function(id) {
+        return users[id] || null;
+    });
+});
