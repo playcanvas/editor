7719b380a99db5b58d225ef800f7a4c321fc47fb
Vaios Kalpias Ilias <vaios@playcanvas.com>
2017-02-17 17:51:36 +0200

    Fix big delays when linting files with lots of errors

--- editor/public/js/codemirror/addon/lint/lint.js
+++ editor/public/js/codemirror/addon/lint/lint.js
@@ -127,38 +127,43 @@
   }
 
   function updateLinting(cm, annotationsNotSorted) {
-    clearMarks(cm);
-    var state = cm.state.lint, options = state.options;
+    // fix by vaios - wrap this in cm.operation for a massive performance boost
+    cm.operation(function () {
+      clearMarks(cm);
+      var state = cm.state.lint, options = state.options;
 
-    var annotations = groupByLine(annotationsNotSorted);
+      var annotations = groupByLine(annotationsNotSorted);
 
-    for (var line = 0; line < annotations.length; ++line) {
-      var anns = annotations[line];
-      if (!anns) continue;
+      for (var line = 0; line < annotations.length; ++line) {
+        var anns = annotations[line];
+        if (!anns) continue;
 
-      var maxSeverity = null;
-      var tipLabel = state.hasGutter && document.createDocumentFragment();
+        var maxSeverity = null;
+        var tipLabel = state.hasGutter && document.createDocumentFragment();
 
-      for (var i = 0; i < anns.length; ++i) {
-        var ann = anns[i];
-        var severity = ann.severity;
-        if (!severity) severity = "error";
-        maxSeverity = getMaxSeverity(maxSeverity, severity);
+        for (var i = 0; i < anns.length; ++i) {
+          var ann = anns[i];
+          var severity = ann.severity;
+          if (!severity) severity = "error";
+          maxSeverity = getMaxSeverity(maxSeverity, severity);
 
-        if (options.formatAnnotation) ann = options.formatAnnotation(ann);
-        if (state.hasGutter) tipLabel.appendChild(annotationTooltip(ann));
+          if (options.formatAnnotation) ann = options.formatAnnotation(ann);
+          if (state.hasGutter) tipLabel.appendChild(annotationTooltip(ann));
 
-        if (ann.to) state.marked.push(cm.markText(ann.from, ann.to, {
-          className: "CodeMirror-lint-mark-" + severity,
-          __annotation: ann
-        }));
+          if (ann.to) state.marked.push(cm.markText(ann.from, ann.to, {
+            className: "CodeMirror-lint-mark-" + severity,
+            __annotation: ann
+          }));
+        }
+
+        if (state.hasGutter)
+          cm.setGutterMarker(line, GUTTER_ID, makeMarker(tipLabel, maxSeverity, anns.length > 1,
+                                                         state.options.tooltips));
       }
+      if (options.onUpdateLinting) options.onUpdateLinting(annotationsNotSorted, annotations, cm);
 
-      if (state.hasGutter)
-        cm.setGutterMarker(line, GUTTER_ID, makeMarker(tipLabel, maxSeverity, anns.length > 1,
-                                                       state.options.tooltips));
-    }
-    if (options.onUpdateLinting) options.onUpdateLinting(annotationsNotSorted, annotations, cm);
+    });
+
   }
 
   function onChange(cm) {
