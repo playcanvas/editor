25183a2c57a59a30fb3193e153db9148226fedc4
Makism Mihejevs <core@moka.co>
2017-03-31 15:55:23 +0100

    light shadow type UI and ref changes for new PCF constants

--- editor/public/js/editor/attributes/components/attributes-components-light.js
+++ editor/public/js/editor/attributes/components/attributes-components-light.js
@@ -373,8 +373,8 @@ editor.once('load', function() {
             type: 'number',
             enum: [
                 { v: '', t: '...' },
-                { v: 0, t: 'Shadow Map 3x3' },
-                { v: 4, t: 'Shadow Map 5x5' },
+                { v: 0, t: 'Shadow Map PCF 3x3' },
+                { v: 4, t: 'Shadow Map PCF 5x5' },
                 { v: 1, t: 'Variance Shadow Map (8bit)' },
                 { v: 2, t: 'Variance Shadow Map (16bit)' },
                 { v: 3, t: 'Variance Shadow Map (32bit)' }
