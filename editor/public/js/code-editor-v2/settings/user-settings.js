ac8de79af7b75c48e6d1a56c716a17a7cbfebca5
Vaios Kalpias Ilias <vaios@playcanvas.com>
2017-04-05 15:47:05 +0300

    IDE settings are now using settings from sharejs

---
+++ editor/public/js/code-editor-v2/settings/user-settings.js
@@ -0,0 +1,71 @@
+editor.once('load', function () {
+    'use strict';
+
+    // settings observer
+    var settings = new Observer({
+        ide: {
+            fontSize: 12,
+            continueComments: true,
+            autoCloseBrackets: true,
+            highlightBrackets: true
+        }
+    });
+
+    // Get settigns
+    editor.method('editor:settings', function () {
+        return settings;
+    });
+
+    var doc;
+
+    editor.on('realtime:authenticated', function () {
+        if (doc) {
+            doc.resume();
+            return;
+        }
+
+        var connection = editor.call('realtime:connection');
+        doc = connection.get('settings', 'user_' + config.self.id);
+
+        // handle errors
+        doc.on('error', function (err) {
+            editor.emit('settings:error', err);
+        });
+
+        // load settings
+        doc.on('subscribe', function () {
+            doc.whenReady(function () {
+                var data = doc.getSnapshot();
+                for (var key in data) {
+                    settings.set(key, data[key]);
+                }
+
+                // server -> local
+                doc.on('after op', function (ops, local) {
+                    if (local) return;
+
+                    for (var i = 0; i < ops.length; i++) {
+                        settings.sync.write(ops[i]);
+                    }
+                });
+            });
+        });
+
+        // subscribe for realtime events
+        doc.subscribe();
+    });
+
+    // local -> server
+    settings.sync = new ObserverSync({
+        item: settings,
+        paths: Object.keys(settings._data)
+    });
+    settings.sync.on('op', function (op) {
+        doc.submitOp([ op ]);
+    });
+
+    editor.on('realtime:disconnected', function () {
+        if (doc)
+            doc.pause();
+    });
+});
