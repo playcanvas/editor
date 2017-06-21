d7ab78ee39de17f5a0bf5828432e05ca58ed8064
Vaios Kalpias Ilias <vaios@playcanvas.com>
2017-06-21 12:17:48 +0300

    Remove null or non existing assets from script order when you inspect it

--- editor/public/js/editor/project/project-scripts-order.js
+++ editor/public/js/editor/project/project-scripts-order.js
@@ -36,7 +36,6 @@ editor.once('load', function() {
         });
     });
 
-
     editor.on('attributes:inspect[editorSettings]', function() {
         var events = [ ];
 
@@ -371,17 +370,24 @@ editor.once('load', function() {
         // get assets
         var assets = projectSettings.get('scripts') || [ ];
 
-        // remove null assets
-        var i = assets.length;
-        while(i--) {
-            if (assets[i] === null)
-                projectSettings.remove('scripts', i);
+        // remove null assets or assets that do not exist
+        if (editor.call('permissions:write')) {
+            var history = projectSettings.history;
+            projectSettings.history = false;
+            var i = assets.length;
+            while (i--) {
+                var id = assets[i];
+                var asset = id ? editor.call('assets:get', id) : null;
+                if (! asset) {
+                    projectSettings.remove('scripts', i);
+                }
+            }
+            projectSettings.history = history;
         }
 
         // add assets
         for(var i = 0; i < assets.length; i++)
             assetAdd(editor.call('assets:get', assets[i]));
-
 
         // on add
         events.push(projectSettings.on('scripts:insert', function(assetId, ind) {
