27fec216e84603b5d7cf7d0bdca8a3f140a02992
Vaios Kalpias Ilias <vaios@playcanvas.com>
2018-03-19 17:04:17 -0700

    Drag and drop sprite assets in Editor viewport

---
+++ editor/public/js/editor/viewport/viewport-drop-sprite.js
@@ -0,0 +1,234 @@
+editor.once('load', function() {
+    'use strict';
+
+    var canvas = editor.call('viewport:canvas');
+    if (! canvas) return;
+
+    var app = editor.call('viewport:app');
+    if (! app) return; // webgl not available
+
+    var aabb = new pc.BoundingBox();
+    var vecA = new pc.Vec3();
+    var vecB = new pc.Vec3();
+    var vecC = new pc.Vec3();
+
+
+    var dropRef = editor.call('drop:target', {
+        ref: canvas.element,
+        filter: function(type, data) {
+            if (type === 'asset.sprite') {
+                var asset = app.assets.get(data.id);
+                if (asset) app.assets.load(asset);
+
+                return true;
+            }
+
+            if (type === 'assets') {
+                for(var i = 0; i < data.ids.length; i++) {
+                    var asset = editor.call('assets:get', data.ids[i]);
+                    if (! asset)
+                        return false;
+
+                    if (asset.get('type') !== 'sprite')
+                        return false;
+                }
+
+                for(var i = 0; i < data.ids.length; i++) {
+                    var asset = app.assets.get(data.ids[i]);
+                    if (asset) app.assets.load(asset);
+                }
+
+                return true;
+            }
+        },
+        drop: function(type, data) {
+            if (! config.scene.id)
+                return;
+
+            var assets = [ ];
+
+            if (type === 'asset.sprite') {
+                var asset = editor.call('assets:get', parseInt(data.id, 10));
+                if (asset) assets.push(asset);
+            } else if (type === 'assets') {
+                for(var i = 0; i < data.ids.length; i++) {
+                    var asset = editor.call('assets:get', parseInt(data.ids[i], 10));
+                    if (asset && asset.get('type') === 'sprite')
+                        assets.push(asset);
+                }
+            }
+
+            if (! assets.length)
+                return;
+
+            // parent
+            var parent = null;
+            if (editor.call('selector:type') === 'entity')
+                parent = editor.call('selector:items')[0];
+
+            if (! parent)
+                parent = editor.call('entities:root');
+
+            var entities = [ ];
+            var data = [ ];
+
+            // calculate aabb
+            var first = true;
+            for(var i = 0; i < assets.length; i++) {
+                var assetEngine = app.assets.get(assets[i].get('id'));
+                if (! assetEngine) continue;
+
+                if (assetEngine.resource) {
+                    var mi = assetEngine.resource._meshInstance;
+                    if (! mi) continue;
+
+                    if (first) {
+                        first = false;
+                        aabb.copy(mi.aabb);
+                    } else {
+                        aabb.add(mi.aabb);
+                    }
+                }
+            }
+
+            if (first) {
+                aabb.center.set(0, 0, 0);
+                aabb.halfExtents.set(1, 1, 1);
+            }
+
+            // calculate point
+            var camera = editor.call('camera:current');
+            var distance = 0;
+
+            if (ui.Tree._ctrl && ui.Tree._ctrl()) {
+                vecA.copy(camera.forward).scale(aabb.halfExtents.length() * 2.2);
+                vecB.copy(camera.getPosition()).add(vecA);
+                vecC.copy(vecB).sub(aabb.center);
+
+                var tmp = new pc.Entity();
+                parent.entity.addChild(tmp);
+                tmp.setPosition(vecC);
+                vecC.copy(tmp.getLocalPosition());
+                tmp.destroy();
+
+                // focus distance
+                distance = vecA.copy(camera.getPosition()).sub(vecB).length();
+            } else {
+                vecC.set(0, 0, 0);
+                vecB.copy(parent.entity.getPosition()).add(aabb.center);
+                distance = aabb.halfExtents.length() * 2.2;
+            }
+
+            for(var i = 0; i < assets.length; i++) {
+                var component = editor.call('components:getDefault', 'sprite');
+
+                var name = assets[i].get('name') || 'Untitled';
+
+                if (assets[i].get('data.frameKeys').length > 1) {
+                    component.type = 'animated';
+                    component.clips = {
+                        '0': {
+                            name: name,
+                            fps: 10,
+                            loop: true,
+                            autoPlay: true,
+                            spriteAsset: parseInt(assets[i].get('id'), 10)
+                        }
+                    };
+                    component.autoPlayClip = name;
+                } else {
+                    component.spriteAsset =  parseInt(assets[i].get('id'), 10);
+                }
+
+                // new entity
+                var entity = editor.call('entities:new', {
+                    parent: parent,
+                    name: name,
+                    position: [ vecC.x, vecC.y, vecC.z ],
+                    components: {
+                        sprite: component
+                    },
+                    noSelect: true,
+                    noHistory: true
+                });
+
+                entities.push(entity);
+                data.push(entity.json());
+            }
+
+            editor.call('selector:history', false);
+            editor.call('selector:set', 'entity', entities);
+            editor.once('selector:change', function() {
+                editor.call('selector:history', true);
+            });
+
+            var selectorType = editor.call('selector:type');
+            var selectorItems = editor.call('selector:items');
+            if (selectorType === 'entity') {
+                for(var i = 0; i < selectorItems.length; i++)
+                    selectorItems[i] = selectorItems[i].get('resource_id');
+            }
+
+            var parentId = parent.get('resource_id');
+            var resourceIds = [ ];
+            for(var i = 0; i < entities.length; i++)
+                resourceIds.push(entities[i].get('resource_id'));
+
+            editor.call('history:add', {
+                name: 'new sprite entities ' + entities.length,
+                undo: function() {
+                    for(var i = 0; i < resourceIds.length; i++) {
+                        var entity = editor.call('entities:get', resourceIds[i]);
+                        if (! entity)
+                            continue;
+
+                        editor.call('entities:removeEntity', entity);
+                    }
+
+                    if (selectorType === 'entity' && selectorItems.length) {
+                        var items = [ ];
+                        for(var i = 0; i < selectorItems.length; i++) {
+                            var item = editor.call('entities:get', selectorItems[i]);
+                            if (item)
+                                items.push(item);
+                        }
+
+                        if (items.length) {
+                            editor.call('selector:history', false);
+                            editor.call('selector:set', selectorType, items);
+                            editor.once('selector:change', function() {
+                                editor.call('selector:history', true);
+                            });
+                        }
+                    }
+                },
+                redo: function() {
+                    var parent = editor.call('entities:get', parentId);
+                    if (! parent)
+                        return;
+
+                    var entities = [ ];
+
+                    for(var i = 0; i < data.length; i++) {
+                        var entity = new Observer(data[i]);
+                        entities.push(entity);
+                        editor.call('entities:childToParent', entity, parent);
+                        editor.call('entities:addEntity', entity, parent, false);
+                    }
+
+                    editor.call('selector:history', false);
+                    editor.call('selector:set', 'entity', entities);
+                    editor.once('selector:change', function() {
+                        editor.call('selector:history', true);
+                    });
+
+                    editor.call('viewport:render');
+                    editor.call('camera:focus', vecB, distance);
+                }
+            });
+
+            editor.call('viewport:render');
+            editor.call('camera:focus', vecB, distance);
+        }
+    });
+});
