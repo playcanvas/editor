16776e42542a257dc347f464f1fb6cce83c34733
Vaios Kalpias Ilias <vaios@playcanvas.com>
2018-11-20 14:37:18 -0800

    Fix some cases of redo() when adding assets to bundles

--- editor/public/js/editor/assets/assets-bundle.js
+++ editor/public/js/editor/assets/assets-bundle.js
@@ -3,6 +3,15 @@ editor.once('load', function () {
 
     var INVALID_TYPES = ['script', 'folder', 'bundle'];
 
+    var isAssetValid = function (asset, existingAssetIds) {
+        var id = asset.get('id');
+        if (existingAssetIds.indexOf(id) !== -1) return false;
+        if (asset.get('source')) return false;
+        if (INVALID_TYPES.indexOf(asset.get('type')) !== -1) return false;
+
+        return true;
+    };
+
     /**
      * Adds assets to the bundle asset. Does not add already existing
      * assets or assets with invalid types.
@@ -12,12 +21,7 @@ editor.once('load', function () {
     editor.method('assets:bundle:addAssets', function (assets, bundleAsset) {
         var existingAssetIds = bundleAsset.get('data.assets');
         var validAssets = assets.filter(function (asset) {
-            var id = asset.get('id');
-            if (existingAssetIds.indexOf(id) !== -1) return false;
-            if (asset.get('source')) return false;
-            if (INVALID_TYPES.indexOf(asset.get('type')) !== -1) return false;
-
-            return true;
+            return isAssetValid(asset, existingAssetIds);
         });
 
         if (! validAssets.length) return;
@@ -38,15 +42,24 @@ editor.once('load', function () {
             var asset = editor.call('assets:get', bundleAsset.get('id'));
             if (! asset) return;
 
+            existingAssetIds = asset.get('data.assets');
+
             var history = asset.history.enabled;
             asset.history.enabled = false;
             for (var i = 0; i < validAssets.length; i++) {
-                asset.insert('data.assets', validAssets[i].get('id'));
+                if (isAssetValid(validAssets[i], existingAssetIds)) {
+                    asset.insert('data.assets', validAssets[i].get('id'));
+                }
             }
             asset.history.enabled = history;
         };
 
-        redo();
+        var history = bundleAsset.history.enabled;
+        bundleAsset.history.enabled = false;
+        for (var i = 0; i < validAssets.length; i++) {
+            bundleAsset.insert('data.assets', validAssets[i].get('id'));
+        }
+        bundleAsset.history.enabled = history;
 
         editor.call('history:add', {
             name: 'asset.' + bundleAsset.get('id') + '.data.assets',
