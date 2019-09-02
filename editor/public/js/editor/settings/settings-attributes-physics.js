e3b88a7a07a56d26b0b556a1a11d173f64bcdb48
Donovan Hutchence <dhutchence@snapchat.com>
2019-09-02 12:27:35 +0100

    added temporary project flag

--- editor/public/js/editor/settings/settings-attributes-physics.js
+++ editor/public/js/editor/settings/settings-attributes-physics.js
@@ -6,6 +6,9 @@ editor.once('load', function() {
 
     var folded = true;
 
+    // TODO: this flag should come from a flag on the project
+    var useAmmoModule = true;
+
     editor.on('attributes:inspect[editorSettings]', function() {
         // physics
         var physicsPanel = editor.call('attributes:addPanel', {
@@ -18,14 +21,16 @@ editor.once('load', function() {
         physicsPanel.class.add('component');
 
         // enable 3d physics
-        var fieldPhysics = editor.call('attributes:addField', {
-            parent: physicsPanel,
-            name: 'Enable',
-            type: 'checkbox',
-            link: projectSettings,
-            path: 'use3dPhysics'
-        });
-        editor.call('attributes:reference:attach', 'settings:project:physics', fieldPhysics.parent.innerElement.firstChild.ui);
+        if (!useAmmoModule) {
+            var fieldPhysics = editor.call('attributes:addField', {
+                parent: physicsPanel,
+                name: 'Enable',
+                type: 'checkbox',
+                link: projectSettings,
+                path: 'use3dPhysics'
+            });
+            editor.call('attributes:reference:attach', 'settings:project:physics', fieldPhysics.parent.innerElement.firstChild.ui);
+        }
 
         // gravity
         var fieldGravity = editor.call('attributes:addField', {
@@ -41,43 +46,45 @@ editor.once('load', function() {
         // reference
         editor.call('attributes:reference:attach', 'settings:gravity', fieldGravity[0].parent.innerElement.firstChild.ui);
 
-        // add ammo module
-        var buttonPanel = new ui.Panel();
-        buttonPanel.class.add('flex', 'component');
-        physicsPanel.append(buttonPanel);
+        // add ammo module button
+        if (useAmmoModule) {
+            var buttonPanel = new ui.Panel();
+            buttonPanel.class.add('flex', 'component');
+            physicsPanel.append(buttonPanel);
 
-        var button = new ui.Button({
-            text: 'Import Ammo modules into this project'
-        });
-        buttonPanel.append(button);
-
-        var tooltipText = "Import asm.js and wasm.js modules into this project from the Playcanvas Store";
-
-        Tooltip.attach({
-            target: button.element,
-            html:  tooltipText,
-            align: 'right',
-            root: editor.call('layout.root')
-        });
-
-        button.on('click', function() {
-            Ajax( {
-                url:'{{url.api}}/store/items?name=Standard Ammo Modules',
-                method:'GET',
-                auth: true,
-                data: { }
-            }).on('load', function(status, data) {
-                if (data.length === 1) {
-                    Ajax( {
-                        url:'{{url.api}}/store/' + data[0].id.toString() + '/clone',
-                        method: 'POST',
-                        auth: true,
-                        data: { scope: { type: 'project', id: config.project.id } }
-                    } ).on('load', function(status, data) {
-                        // done
-                    });
-                }
+            var button = new ui.Button({
+                text: 'Import Ammo modules into this project'
             });
-        });
+            buttonPanel.append(button);
+
+            var tooltipText = "Import asm.js and wasm.js modules into this project from the Playcanvas Store";
+
+            Tooltip.attach({
+                target: button.element,
+                html:  tooltipText,
+                align: 'right',
+                root: editor.call('layout.root')
+            });
+
+            button.on('click', function() {
+                Ajax( {
+                    url:'{{url.api}}/store/items?name=Standard Ammo Modules',
+                    method:'GET',
+                    auth: true,
+                    data: { }
+                }).on('load', function(status, data) {
+                    if (data.length === 1) {
+                        Ajax( {
+                            url:'{{url.api}}/store/' + data[0].id.toString() + '/clone',
+                            method: 'POST',
+                            auth: true,
+                            data: { scope: { type: 'project', id: config.project.id } }
+                        } ).on('load', function(status, data) {
+                            // done
+                        });
+                    }
+                });
+            });
+        }
     });
 });
