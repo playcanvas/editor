d09c6d1f86ecfd69de40aa92d8fcd3d80f6ea674
Donovan Hutchence <dhutchence@snapchat.com>
2019-09-02 12:41:49 +0100

    small changes

--- editor/public/js/editor/settings/settings-attributes-physics.js
+++ editor/public/js/editor/settings/settings-attributes-physics.js
@@ -6,8 +6,8 @@ editor.once('load', function() {
 
     var folded = true;
 
-    // TODO: this flag should come from a flag on the project
-    var useAmmoModule = true;
+    // TODO: this flag should come from the project settings
+    var enableLegacyAmmo = true;
 
     editor.on('attributes:inspect[editorSettings]', function() {
         // physics
@@ -21,7 +21,7 @@ editor.once('load', function() {
         physicsPanel.class.add('component');
 
         // enable 3d physics
-        if (!useAmmoModule) {
+        if (enableLegacyAmmo) {
             var fieldPhysics = editor.call('attributes:addField', {
                 parent: physicsPanel,
                 name: 'Enable',
@@ -46,45 +46,43 @@ editor.once('load', function() {
         // reference
         editor.call('attributes:reference:attach', 'settings:gravity', fieldGravity[0].parent.innerElement.firstChild.ui);
 
-        // add ammo module button
-        if (useAmmoModule) {
-            var buttonPanel = new ui.Panel();
-            buttonPanel.class.add('flex', 'component');
-            physicsPanel.append(buttonPanel);
+        // ammo module button
+        var buttonPanel = new ui.Panel();
+        buttonPanel.class.add('flex', 'component');
+        physicsPanel.append(buttonPanel);
 
-            var button = new ui.Button({
-                text: 'Import Ammo modules into this project'
+        var button = new ui.Button({
+            text: 'Import Ammo into this project'
+        });
+        buttonPanel.append(button);
+
+        var tooltipText = "Add the Ammo asm.js and wasm modules to this project from the Playcanvas Store";
+
+        Tooltip.attach({
+            target: button.element,
+            html:  tooltipText,
+            align: 'right',
+            root: editor.call('layout.root')
+        });
+
+        button.on('click', function() {
+            Ajax( {
+                url:'{{url.api}}/store/items?name=Ammo',
+                method:'GET',
+                auth: true,
+                data: { }
+            }).on('load', function(status, data) {
+                if (data.length === 1) {
+                    Ajax( {
+                        url:'{{url.api}}/store/' + data[0].id.toString() + '/clone',
+                        method: 'POST',
+                        auth: true,
+                        data: { scope: { type: 'project', id: config.project.id } }
+                    } ).on('load', function(status, data) {
+                        // done
+                    });
+                }
             });
-            buttonPanel.append(button);
-
-            var tooltipText = "Import asm.js and wasm.js modules into this project from the Playcanvas Store";
-
-            Tooltip.attach({
-                target: button.element,
-                html:  tooltipText,
-                align: 'right',
-                root: editor.call('layout.root')
-            });
-
-            button.on('click', function() {
-                Ajax( {
-                    url:'{{url.api}}/store/items?name=Ammo',
-                    method:'GET',
-                    auth: true,
-                    data: { }
-                }).on('load', function(status, data) {
-                    if (data.length === 1) {
-                        Ajax( {
-                            url:'{{url.api}}/store/' + data[0].id.toString() + '/clone',
-                            method: 'POST',
-                            auth: true,
-                            data: { scope: { type: 'project', id: config.project.id } }
-                        } ).on('load', function(status, data) {
-                            // done
-                        });
-                    }
-                });
-            });
-        }
+        });
     });
 });
