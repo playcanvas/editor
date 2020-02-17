7e524112d3601a9b59cfc39065c7bd5e654493ff
Elliott Thompson <elliott@moo.com>
2020-02-17 11:44:55 +0000

    split the cubemap face out into its own file and add a cubemap preview class

--- editor/public/js/editor/inspector/assets/cubemap.js
+++ editor/public/js/editor/inspector/assets/cubemap.js
@@ -3,9 +3,6 @@ Object.assign(pcui, (function () {
 
     const CLASS_ROOT = 'pcui-cubemap-asset-inspector';
     const CLASS_FACES_CONTAINER = CLASS_ROOT + '-faces-container';
-    const CLASS_FACE = CLASS_ROOT + '-face';
-    const CLASS_FACE_LABEL = CLASS_FACE + '-label';
-    const CLASS_FACE_BUTTON = CLASS_FACE + '-button';
 
     const ATTRIBUTES = [{
         label: 'Filtering',
@@ -36,22 +33,6 @@ Object.assign(pcui, (function () {
         4: 'Front',
         5: 'Back'
     };
-
-    const DOM_CUBEMAP_FACE = (args) => [
-        {
-            thumbnail: new pcui.AssetThumbnail({
-                assets: args.assets,
-                width: '100%',
-                binding: new pcui.BindingTwoWay({
-                    history: args.history
-                })
-            })
-        }, {
-            deleteButton: new pcui.Button({ icon: 'E124', class: CLASS_FACE_BUTTON })
-        }, {
-            faceLabel: new pcui.Label({ text: args.label, class: CLASS_FACE_LABEL })
-        }
-    ];
 
     const DOM = (parent, args) => [
         {
@@ -104,108 +85,6 @@ Object.assign(pcui, (function () {
         }
     ];
 
-    class CubemapFace extends pcui.Container {
-        constructor(args) {
-            args = Object.assign({}, args);
-
-            super(args);
-            this._args = args;
-            this._asset = null;
-            this._assetEvents = [];
-
-            this.buildDom(DOM_CUBEMAP_FACE(args));
-            this.class.add(CLASS_FACE);
-            this.class.add(args.label);
-        }
-
-        _setRgbmIfNeeded() {
-            let allHdr = true;
-            const textures = this._asset.get('data.textures');
-            for (let i = 0; i < textures.length; i++) {
-                if (textures[i] >= 0) {
-                    const texture = editor.call('assets:get', textures[i]);
-                    if (texture && !texture.get('data.rgbm')) {
-                        allHdr = false;
-                        break;
-                    }
-                }
-            }
-            if (allHdr)  {
-                this._asset.set('data.rgbm', true);
-            } else {
-                this._asset.unset('data.rgbm');
-            }
-        }
-
-        _onClickFace() {
-            if (! editor.call('permissions:write'))
-                return;
-
-            const texture = editor.call('assets:get', this._asset.get(`data.textures.${this._args.face}`));
-            editor.call('picker:asset', {
-                type: 'texture',
-                currentAsset: texture
-            });
-
-            let evtPick = editor.once('picker:asset', (texture) => {
-                this._asset.set(`data.textures.${this._args.face}`, texture.get('id'));
-                evtPick = null;
-            });
-
-            editor.once('picker:asset:close', () => {
-                if (evtPick) {
-                    evtPick.unbind();
-                    evtPick = null;
-                }
-            });
-        }
-
-        _onClickDeleteFace() {
-            this._asset.set(`data.textures.${this._args.face}`, null);
-        }
-
-        _initialiseDropTarget() {
-            editor.call('drop:target', {
-                ref: this._thumbnail,
-                filter: (type, dropData) => {
-                    if (dropData.id && type.startsWith('asset') &&
-                        (type === 'asset.texture') &&
-                        parseInt(dropData.id, 10) !== this._asset.get(`data.textures.${this._args.face}`)) {
-                        const asset = this._args.assets.get(dropData.id);
-                        return !!asset && !asset.get('source');
-                    }
-                },
-                drop: (type, dropData) => {
-                    this._asset.set(`data.textures.${this._args.face}`, parseInt(dropData.id, 10));
-                    this._setRgbmIfNeeded();
-                }
-            });
-        }
-
-        link(asset, path) {
-            this._asset = asset;
-            this.unlink();
-            this._thumbnail.link(asset, path);
-            this._initialiseDropTarget();
-            this._assetEvents.push(this._deleteButton.on('click', this._onClickDeleteFace.bind(this)));
-            this._assetEvents.push(this.on('click', this._onClickFace.bind(this)));
-            this._assetEvents.push(this._asset.on('*:set', () => {
-                this._deleteButton.hidden = !this._asset.get(`data.textures.${this._args.face}`);
-            }));
-            this._assetEvents.push(this._asset.on('*:unset', () => {
-                this._deleteButton.hidden = !this._asset.get(`data.textures.${this._args.face}`);
-            }));
-            this.hidden = false;
-            this._deleteButton.hidden = !this._asset.get(`data.textures.${this._args.face}`);
-        }
-
-        unlink() {
-            if (!this._asset) return;
-            this._thumbnail.unlink();
-            this._assetEvents.forEach(evt => evt.unbind());
-        }
-    }
-
     class CubemapAssetInspector extends pcui.Container {
         constructor(args) {
             args = Object.assign({}, args);
@@ -219,7 +98,7 @@ Object.assign(pcui, (function () {
             this.buildDom(DOM(this, args));
 
             Object.keys(FACES).forEach(face => {
-                const cubemapFace = new CubemapFace(Object.assign(args, { label: FACES[face], face }));
+                const cubemapFace = new pcui.CubemapFace(Object.assign(args, { label: FACES[face], face }));
                 this._faces.push(cubemapFace);
                 this._facesPanel.append(cubemapFace);
             });
