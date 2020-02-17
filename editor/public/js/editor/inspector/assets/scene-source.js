b2ca348e8e5e105f9a2ca80e21de52db3eb9cfda
Elliott Thompson <elliott@moo.com>
2020-02-17 10:16:30 +0000

    fix scene content events

--- editor/public/js/editor/inspector/assets/scene-source.js
+++ editor/public/js/editor/inspector/assets/scene-source.js
@@ -113,22 +113,23 @@ Object.assign(pcui, (function () {
             this._animationCheck(assets[0].get('meta.animation.available'));
             this._assetEvents.push(assets[0].on('meta.animation.available:set', this._animationCheck.bind(this)));
             this._addTextures(assets[0].get('meta.textures'));
+            this._addMaterials(assets[0].get('meta.materials'));
             this._assetEvents.push(assets[0].on('meta.textures:set', () => {
                 this._removeTextures();
                 this.addTextures(assets[0].get('meta.textures'));
             }));
             this._assetEvents.push(assets[0].on('meta.textures:unset', () => {
                 this._removeTextures();
-                this.addTextures(assets[0].get('meta.textures'));
+                this._addTextures(assets[0].get('meta.textures'));
             }));
             this._addMaterials(assets[0].get('meta.materials'));
             this._assetEvents.push(assets[0].on('meta.materials:set', () => {
                 this._removeMaterials();
-                this.addMaterials(assets[0].get('meta.materials'));
+                this._addMaterials(assets[0].get('meta.materials'));
             }));
             this._assetEvents.push(assets[0].on('meta.materials:unset', () => {
                 this._removeMaterials();
-                this.addMaterials(assets[0].get('meta.materials'));
+                this._addMaterials(assets[0].get('meta.materials'));
             }));
         }
 
