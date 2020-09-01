9a9912786b005b529b581609ab7f54a9587c5346
Vaios Kalpias Ilias <vkalpiasilias@snapchat.com>
2020-09-01 13:52:24 -0700

    Allow duplicates when using observer.set with arrays of values (#85)

--- editor/public/js/observer.js
+++ editor/public/js/observer.js
@@ -322,7 +322,7 @@ Observer.prototype.set = function(path, value, silent, remote, force) {
             } else {
                 node._data[key] = [];
                 value.forEach(val => {
-                    this._doInsert(node, key, val);
+                    this._doInsert(node, key, val, undefined, true);
                 });
 
                 state = obj.silence();
@@ -723,7 +723,7 @@ Observer.prototype.insert = function(path, value, ind, silent, remote) {
     return true;
 };
 
-Observer.prototype._doInsert = function(node, key, value, ind) {
+Observer.prototype._doInsert = function(node, key, value, ind, allowDuplicates) {
     const arr = node._data[key];
 
     if (typeof(value) === 'object' && ! (value instanceof Observer) && value !== null) {
@@ -735,11 +735,12 @@ Observer.prototype._doInsert = function(node, key, value, ind) {
     }
 
     const path = node._path ? `${node._path}.${key}` : key;
-    if (value !== null && (!this._pathsWithDuplicates || !this._pathsWithDuplicates[path])) {
+    if (value !== null && !allowDuplicates && (!this._pathsWithDuplicates || !this._pathsWithDuplicates[path])) {
         if (arr.indexOf(value) !== -1) {
             return;
         }
     }
+
 
     if (ind === undefined) {
         arr.push(value);
