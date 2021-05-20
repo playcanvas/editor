d3e8634118e8fe69dc3c59a7425feb737e462139
Zachary Paul <zpaul@snapchat.com>
2021-05-20 07:51:36 -0700

    Handle script and entity attribute updates after script parse on the backend (#759)
    * script task initial
    
    * refactor run task
    
    * load data
    
    * loading conflict
    
    * rm debug
    
    * await
    
    * rename to update script asset
    
    * return val
    
    * rm comments
    
    * rm script conflicts
    
    * space
    
    * handle script attrs
    
    * rm log
    
    * move partition
    
    * use new class
    
    * attr order
    
    * rename fun
    
    * rm unused editor code
    
    * const var
    
    * init for existing scripts move
    
    * handle parse init
    
    * fixes
    
    * use common code
    
    * todo comment
    
    * template scenes init
    
    * move convert attrs
    
    * rm deep clone orig asset data
    
    * rm unused
    
    * get node
    
    * group ents
    
    * rename fun
    
    * rename var
    
    * init ent scr attr
    
    * rm attrs cons
    
    * init is def needed
    
    * init is def needed class
    
    * init is def needed logic
    
    * comment
    
    * rename defin
    
    * init default val
    
    * decl to type const
    
    * move const to utils
    
    * rename const
    
    * decl default
    
    * def
    
    * init def val complete
    
    * rename var
    
    * rename var
    
    * is equal def
    
    * rm both ars
    
    * finish is def needed
    
    * rm unused
    
    * no attrs case
    
    * empty obj param
    
    * attr for json
    
    * move ar check
    
    * rm editor code
    
    * copy master ent copy app file
    
    * script attrs task
    
    * rm space
    
    * move new scr
    
    * split init
    
    * move set scr asset
    
    * scrAssetById
    
    * move scr names
    
    * move templ assets
    
    * move find scenes
    
    * move templ assets, tmp scene
    
    * handle scene added
    
    * rename var
    
    * dont call update asset
    
    * add task type
    
    * fix no orig script
    
    * comment
    
    * comment
    
    * comments
    
    * json and equal
    
    * pass old val as dst if no ent attrs
    
    * use var for new val {}
    
    * rm badt var
    
    * rename funs
    
    * rename fun
    
    * 1 or 2 dim ars
    
    * comments added
    
    * rename function
    
    * init script tests
    
    * expect
    
    * expect false
    
    * mod ent
    
    * refactor test ent
    
    * check ent attr
    
    * test keep value
    
    * refactor task
    
    * test add ent task
    
    * test template update
    
    * finish test template
    
    * ent w defaults test
    
    * refactor add ent tests
    
    * enum field in asset
    
    * add enum field to ent
    
    * keep old value, curve added
    
    * move file
    
    * second script
    
    * defaults for all types
    
    * array defaults
    
    * enum in json schema in asset test
    
    * curve w curves attr
    
    * Only start script attribute job if local user added script and has write permissions
    
    * add script attr true to docker-local.json, test.json
    
    * add script attr task to docker local
    
    * add script attrs to onboarding supervisor
    
    Co-authored-by: zachppaul <zachppaul@gmail.com>
    Co-authored-by: Vaios Kalpias Ilias <vkalpiasilias@c.snap.com>

--- editor/public/js/editor/entities/entities-scripts.js
+++ editor/public/js/editor/entities/entities-scripts.js
@@ -1,26 +1,6 @@
 editor.once('load', function () {
     'use strict';
 
-    // Default values per script attribute type
-    var DEFAULTS = utils.deepCopy(pcui.DEFAULTS);
-
-    // The expected typeof value for each script attribute type
-    var TYPES = {
-        boolean: 'boolean',
-        number: 'number',
-        string: 'string',
-        json: 'object',
-        asset: 'number',
-        entity: 'string',
-        rgb: 'object',
-        rgba: 'object',
-        vec2: 'object',
-        vec3: 'object',
-        vec4: 'object',
-        curve: 'object',
-        gradient: 'object'
-    };
-
     // Indexes all entities with scripts
     // When primary script is set, update attributes on entities components
     // When script attributes change, update attributes on entities components
@@ -57,8 +37,6 @@ editor.once('load', function () {
             asset: null,
             events: []
         };
-
-        this._updateAllAttributes(entity, script);
     };
 
     // When the specified script is removed from the specified entity
@@ -70,301 +48,18 @@ editor.once('load', function () {
             return;
 
         var item = index[script][entity.get('resource_id')];
+
         if (!item) return;
 
         for (let i = 0; i < item.events.length; i++)
             item.events[i].unbind();
 
         delete index[script][entity.get('resource_id')];
+
         if (Object.keys(index[script]).length === 0) {
             delete index[script];
         }
-
     };
-
-    EntitiesScriptsIndex.prototype._updateAllAttributes =  function (entity, script) {
-        var asset = editor.call('assets:scripts:assetByScript', script);
-        if (!asset) return;
-
-        var i;
-        var history;
-        var index = this._index;
-        var item = index[script][entity.get('resource_id')];
-        var assetOld = null;
-
-        // clean up old item.asset if it points to a different script asset
-        if (item.asset && item.asset !== asset.get('id')) {
-            assetOld = item.asset;
-
-            for (i = 0; i < item.events.length; i++)
-                item.events[i].unbind();
-
-            item.events = [];
-            item.asset = asset.get('id');
-        }
-
-        // unset attributes
-        var attributes = entity.get('components.script.scripts.' + script + '.attributes');
-        for (const key in attributes) {
-            if (!attributes.hasOwnProperty(key))
-                continue;
-
-            if (!asset.has('data.scripts.' + script + '.attributes.' + key)) {
-                history = entity.history.enabled;
-                entity.history.enabled = false;
-                entity.unset('components.script.scripts.' + script + '.attributes.' + key);
-                entity.history.enabled = history;
-            }
-        }
-
-        // set/update attributes
-        var attributesOrder = asset.get('data.scripts.' + script + '.attributesOrder');
-        for (i = 0; i < attributesOrder.length; i++) {
-            var name = attributesOrder[i];
-            var attribute = asset.get('data.scripts.' + script + '.attributes.' + name);
-
-            var oldAttribute = null;
-            if (assetOld) {
-                oldAttribute = assetOld.get('data.scripts.' + script + '.attributes.' + name);
-            }
-
-            this._setNewDefaultAttributeValueIfNeeded(entity, script, name, attribute, oldAttribute);
-        }
-
-        // subscribe to script asset attribute changes
-        // set attribute
-        var self = this;
-        item.events.push(asset.on('*:set', function (path, attribute, old) {
-            self._onSetAssetField(entity, script, path, attribute, old);
-
-        }));
-        // unset attribute
-        item.events.push(asset.on('*:unset', function (path, attribute) {
-            self._onUnsetAssetField(entity, script, path, attribute);
-        }));
-    };
-
-    EntitiesScriptsIndex.prototype._isNewDefaultValueRequired = function (attribute, oldAttribute, currentValue) {
-        var result = false;
-
-        if (attribute.type !== oldAttribute.type || (!!attribute.array !== !!oldAttribute.array)) {
-            result = true;
-        } else if (currentValue === undefined) {
-            result = true;
-        } else if (attribute.type === 'json' && !this._areEqual(attribute.schema, oldAttribute.schema, 'json')) {
-            result = true;
-        } else {
-            var oldDefaultValue = this._getDefaultAttributeValue(oldAttribute);
-
-            result = this._areEqual(currentValue, oldDefaultValue, attribute.type);
-
-            // curve types
-            if (!result && attribute.type === 'curve') {
-                if (attribute.color || attribute.curves) {
-                    var len = attribute.color ? attribute.color.length : attribute.curves.length;
-                    if (attribute.array) {
-                        if (!currentValue || (!(currentValue instanceof Array))) {
-                            result = true;
-                        } else {
-                            for (var j = 0; j < currentValue.length && !result; j++) {
-                                var val = currentValue[j];
-                                if (len !== 1 && (!(val.keys[0] instanceof Array) || val.keys.length !== len)) {
-                                    result = true;
-                                } else if (len === 1 && (val.keys[0] instanceof Array)) {
-                                    result = true;
-                                }
-                            }
-                        }
-                    } else {
-                        if (len !== 1 && (!(currentValue.keys[0] instanceof Array) || currentValue.keys.length !== len)) {
-                            result = true;
-                        } else if (len === 1 && (currentValue.keys[0] instanceof Array)) {
-                            result = true;
-                        }
-                    }
-
-                } else if (!currentValue || !currentValue.keys || currentValue.keys[0] instanceof Array) {
-                    result = true;
-                }
-            }
-        }
-
-        return result;
-    };
-
-    // Checks if the specified value equals the default attribute value
-    EntitiesScriptsIndex.prototype._areEqual = function (value, defaultValue, type) {
-        if (typeof value !== typeof defaultValue && defaultValue !== null) {
-            return false;
-        }
-
-        if (type === 'json') {
-            try {
-                return JSON.stringify(value) === JSON.stringify(defaultValue);
-            } catch (err) {
-            }
-
-            return false;
-        }
-
-        if ((defaultValue instanceof Array) && (value instanceof Array) && defaultValue.equals(value)) {
-            // was default array value
-            return true;
-        } else if ((defaultValue instanceof Object) && (value instanceof Object) && defaultValue.type === value.type && (defaultValue.keys instanceof Array) && (value.keys instanceof Array) && defaultValue.keys.length === value.keys.length) {
-            if ((defaultValue.keys[0] instanceof Array) && (value.keys[0] instanceof Array)) {
-                for (var k = 0; k < defaultValue.keys.length; k++) {
-                    if (!defaultValue.keys[k].equals(value.keys[k])) {
-                        // was curveset default value
-                        return false;
-                    }
-                }
-                return true;
-            } else if (defaultValue.keys.equals(value.keys)) {
-                // was curve default value
-                return true;
-            }
-        } else if (defaultValue === value) {
-            // was default value
-            return true;
-        }
-        return false;
-    };
-
-    EntitiesScriptsIndex.prototype._getNewDefaultValueForJsonFieldIfNeeded = function (attribute, oldAttribute, currentValue) {
-        const result = this._getDefaultAttributeValue(attribute);
-
-        const newSchemaIndex = {};
-        attribute.schema.forEach(field => {
-            newSchemaIndex[field.name] = field;
-        });
-
-        // go through all fields from the old schema and if they exist
-        // in the new value update those values to the new values
-        oldAttribute.schema.forEach(field => {
-            if (newSchemaIndex.hasOwnProperty(field.name) && currentValue.hasOwnProperty(field.name)) {
-                if (!this._isNewDefaultValueRequired(newSchemaIndex[field.name], field, currentValue[field.name])) {
-                    result[field.name] = utils.deepCopy(currentValue[field.name]);
-                }
-            }
-        });
-
-        return result;
-    };
-
-    EntitiesScriptsIndex.prototype._setNewDefaultAttributeValueIfNeeded = function (entity, script, name, attribute, oldAttribute) {
-        if (! oldAttribute) {
-            oldAttribute = attribute;
-        }
-
-        const attributesPath = `components.script.scripts.${script}.attributes`;
-        var path = `${attributesPath}.${name}`;
-        var currentValue = entity.has(path) ? entity.get(path) : undefined;
-
-        let newDefaultValue;
-
-        if (this._isNewDefaultValueRequired(attribute, oldAttribute, currentValue)) {
-            // in case of json attributes if there was a previous json value we want to
-            // avoid resetting the whole value but instead add / remove / update existing fields
-            if (attribute.type === 'json' && oldAttribute.type === 'json' && attribute.array === oldAttribute.array && currentValue) {
-                if (oldAttribute.array) {
-                    if (currentValue.length) {
-                        newDefaultValue = [];
-
-                        var attributeCopy = Object.assign({}, attribute);
-                        attributeCopy.array = false;
-                        var oldAttributeCopy = Object.assign({}, oldAttribute);
-                        oldAttributeCopy.array = false;
-                        for (let i = 0; i < currentValue.length; i++) {
-                            if (!currentValue[i]) {
-                                newDefaultValue.push(currentValue[i]);
-                                continue;
-                            }
-
-
-                            newDefaultValue.push(this._getNewDefaultValueForJsonFieldIfNeeded(attributeCopy, oldAttributeCopy, currentValue[i]));
-                        }
-                    }
-                } else {
-                    newDefaultValue = this._getNewDefaultValueForJsonFieldIfNeeded(attribute, oldAttribute, currentValue);
-                }
-            }
-
-            var history = entity.history.enabled;
-            entity.history.enabled = false;
-            if (newDefaultValue === undefined) {
-                newDefaultValue = this._getDefaultAttributeValue(attribute);
-            }
-            // fix undefined attributes - if entity has undefined
-            // attributes (not sure how that happens yet) then setting
-            // an attribute will just cause an error. So set attributes to
-            // an empty object first.
-            if (!entity.has(attributesPath)) {
-                entity.set(attributesPath, {});
-            }
-            entity.set(path, newDefaultValue);
-            entity.history.enabled = history;
-        }
-    };
-
-    // Returns a new default value for the specified attribute
-    EntitiesScriptsIndex.prototype._getDefaultAttributeValue = function (attribute) {
-        var value = attribute.array ? [] : null;
-
-        if (attribute.default !== undefined && attribute.default !== null) {
-            if (attribute.array) {
-                if (attribute.default instanceof Array) {
-                    value = attribute.default;
-                    for (let i = 0; i < attribute.default.length; i++) {
-                        if (typeof (attribute.default[i]) !== TYPES[attribute.type]) {
-                            value = [];
-                            break;
-                        }
-                    }
-                }
-            } else if (typeof (attribute.default) === TYPES[attribute.type]) {
-                value = attribute.default;
-            }
-        }
-
-        if (value === null) {
-            value = DEFAULTS[attribute.type];
-            if (value instanceof Array) {
-                value = value.slice(0);
-            } else if (typeof value === 'object') {
-                value = utils.deepCopy(value);
-            }
-
-            if (attribute.type === 'curve') {
-                if (attribute.array) {
-                    value = [];
-                } else {
-                    if (attribute.color || attribute.curves) {
-                        var len = attribute.color ? attribute.color.length : attribute.curves.length;
-                        var v = attribute.color ? 1 : 0;
-                        value.keys = [];
-                        for (var c = 0; c < len; c++) {
-                            value.keys.push([0, v]);
-                        }
-                    }
-                }
-            } else if (attribute.type === 'json') {
-                if (attribute.array) {
-                    value = [];
-                } else {
-                    // handle json attribute by creating a default instance of the schema
-                    if (attribute.schema) {
-                        value = {};
-                        attribute.schema.forEach(field => {
-                            value[field.name] = this._getDefaultAttributeValue(field);
-                        });
-                    }
-                }
-            }
-        }
-
-        return value;
-    };
-
 
     // Called when a new entity is added. Adds the entity to the index
     // and subscribes to component script events
@@ -378,8 +73,12 @@ editor.once('load', function () {
             }
         }
 
-        entity.on('components.script.order:insert', function (script) {
+        entity.on('components.script.order:insert', function (script, index, remote) {
             self.add(entity, script);
+
+            if (!remote && editor.call('permissions:write')) {
+                self.setDefaultAttrs(entity, script);
+            }
         });
 
         entity.on('components.script.order:remove', function (script) {
@@ -397,6 +96,23 @@ editor.once('load', function () {
         });
     };
 
+    EntitiesScriptsIndex.prototype.setDefaultAttrs = function (entity, script) {
+        const h = {
+            script_task_type: 'set_entity_defaults',
+            job_id: editor.call('utils:makeGuid'),
+            project_id: config.project.id,
+            branch_id: config.self.branch.id,
+            script_added_to_ent: script,
+            dst_scene_id: config.scene.id,
+            dst_ent_id: entity.get('resource_id')
+        };
+
+        editor.call('realtime:send', 'pipeline', {
+            name: 'script-attributes',
+            data: h
+        });
+    };
+
     // Called when an entity is removed to remove the entity from the index
     EntitiesScriptsIndex.prototype.onEntityRemove = function (entity) {
         var scripts = entity.get('components.script.order');
@@ -406,54 +122,6 @@ editor.once('load', function () {
                 this.remove(entity, scripts[i]);
             }
         }
-    };
-
-    // Called when the primary script is set
-    EntitiesScriptsIndex.prototype.onSetPrimaryScript = function (asset, script) {
-        var index = this._index;
-        if (!index[script])
-            return;
-
-        for (const key in index[script]) {
-            if (!index[script].hasOwnProperty(key))
-                continue;
-
-            this._updateAllAttributes(index[script][key].entity, script);
-        }
-    };
-
-    // Handles setting the script attributes on a script asset
-    EntitiesScriptsIndex.prototype._onSetAssetField = function (entity, script, path, attribute, oldAttribute) {
-        if (!path.startsWith('data.scripts.' + script + '.attributes.'))
-            return;
-
-        var parts = path.split('.');
-        if (parts.length !== 5)
-            return;
-
-        var name = parts[4];
-
-        this._setNewDefaultAttributeValueIfNeeded(entity, script, name, attribute, oldAttribute);
-    };
-
-    // Handled unsetting script attributes from a script asset
-    EntitiesScriptsIndex.prototype._onUnsetAssetField = function (entity, script, path, attribute) {
-        if (!path.startsWith('data.scripts.' + script + '.attributes.'))
-            return;
-
-        var parts = path.split('.');
-        if (parts.length !== 5)
-            return;
-
-        var name = parts[4];
-
-        if (!entity.has('components.script.scripts.' + script + '.attributes.' + name))
-            return;
-
-        var history = entity.history.enabled;
-        entity.history.enabled = false;
-        entity.unset('components.script.scripts.' + script + '.attributes.' + name);
-        entity.history.enabled = history;
     };
 
     // create a new instance of the index
@@ -466,10 +134,6 @@ editor.once('load', function () {
 
     editor.on('entities:remove', function (entity) {
         entitiesScriptsIndex.onEntityRemove(entity);
-    });
-
-    editor.on('assets:scripts:primary:set', function (asset, script) {
-        entitiesScriptsIndex.onSetPrimaryScript(asset, script);
     });
 
     editor.method('entities:list:byScript', function (script) {
