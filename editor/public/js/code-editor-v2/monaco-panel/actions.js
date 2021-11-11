eccc769b90cda5e0025046e35eaf595497367e4b
Vaios Kalpias Ilias <vkalpiasilias@c.snap.com>
2021-11-11 16:52:15 +0200

    Update keys for monaco actions due to monaco-editor upgrade

--- editor/public/js/code-editor-v2/monaco-panel/actions.js
+++ editor/public/js/code-editor-v2/monaco-panel/actions.js
@@ -7,75 +7,75 @@ editor.once('load', function () {
 
     // custom undo
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_Z, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyZ, () => {
             editor.call('editor:command:undo');
         }
     );
 
     // custom redo
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KEY_Z, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyZ, () => {
             editor.call('editor:command:redo');
         }
     );
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd |  monaco.KeyCode.KEY_Y, () => {
+        monaco.KeyMod.CtrlCmd |  monaco.KeyCode.KeyY, () => {
             editor.call('editor:command:redo');
         }
     );
 
     // close current file
     monacoEditor.addCommand(
-        monaco.KeyMod.Alt | monaco.KeyCode.KEY_W, () => {
+        monaco.KeyMod.Alt | monaco.KeyCode.KeyW, () => {
             editor.call('editor:command:close');
         }
     );
 
     // close all files
     monacoEditor.addCommand(
-        monaco.KeyMod.Alt | monaco.KeyMod.Shift | monaco.KeyCode.KEY_W, () => {
+        monaco.KeyMod.Alt | monaco.KeyMod.Shift | monaco.KeyCode.KeyW, () => {
             editor.call('editor:command:closeAll');
         }
     );
 
     // go to file
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_P, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyP, () => {
             editor.call('editor:command:goToFile');
         }
     );
 
     // next tab
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Alt | monaco.KeyCode.US_DOT, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Alt | monaco.KeyCode.Period, () => {
             editor.call('editor:command:nextTab');
         }
     );
 
     // previous tab
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Alt | monaco.KeyCode.US_COMMA, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Alt | monaco.KeyCode.Comma, () => {
             editor.call('editor:command:previousTab');
         }
     );
 
     // find in files
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KEY_F, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyF, () => {
             editor.call('picker:search:open');
         }
     );
 
     // save
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyCode.KeyS, () => {
             editor.call('editor:command:save');
         }
     );
 
     // command pallette
     monacoEditor.addCommand(
-        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KEY_P, () => {
+        monaco.KeyMod.CtrlCmd | monaco.KeyMod.Shift | monaco.KeyCode.KeyP, () => {
             monacoEditor.focus();
             monacoEditor.trigger(null, 'editor.action.quickCommand');
         }
