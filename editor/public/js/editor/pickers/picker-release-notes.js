786a0a10d99440faf8bde27098217ecb3f9b7481
Vaios Kalpias Ilias <vkalpiasilias@c.snap.com>
2021-12-14 11:28:01 +0200

    Editor: Added release notes button which takes you to github release … (#1199)

---
+++ editor/public/js/editor/pickers/picker-release-notes.js
@@ -0,0 +1,36 @@
+editor.once('load', () => {
+    const LOCALSTORAGE_KEY = 'playcanvas-editor-latest-release-notes';
+    const latestVersionSeen = localStorage.getItem(LOCALSTORAGE_KEY);
+    if (latestVersionSeen === config.version) return;
+
+    const popup = new pcui.Container({
+        class: 'popup-release-notes',
+        flex: true,
+        flexDirection: 'row'
+    });
+    const label = new pcui.Label({
+        text: 'WHAT\'S NEW'
+    });
+    popup.append(label);
+
+    function dismiss() {
+        localStorage.setItem(LOCALSTORAGE_KEY, config.version);
+        popup.destroy();
+    }
+
+    popup.on('click', () => {
+        dismiss();
+        window.open("https://github.com/playcanvas/editor/releases/tag/v" + config.version);
+    });
+
+    const btnDismiss = new pcui.Button({
+        icon: 'E132'
+    });
+    popup.append(btnDismiss);
+    btnDismiss.on('click', (evt) => {
+        evt.stopPropagation();
+        dismiss();
+    });
+
+    editor.call('layout.viewport').append(popup);
+});
