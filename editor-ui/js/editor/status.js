18ec31748f2270d322ab5627fd02bc5cdec8d80c
Will Eastcott <weastcott@snapchat.com>
2022-11-14 11:38:52 +0000

    Migrate status bar to PCUI (#1690)
    * Migrate status bar to PCUI
    
    * No need to specify redundant properties
    
    * Move width of progress bar to CSS
    
    Co-authored-by: Will Eastcott <will@playcanvas.com>

--- editor-ui/js/editor/status.js
+++ editor-ui/js/editor/status.js
@@ -1,28 +1,23 @@
 editor.once('load', function () {
     'use strict';
 
-    var jobs = { };
-    var panel = editor.call('layout.statusBar');
-
+    const jobs = { };
+    const panel = editor.call('layout.statusBar');
 
     // status
-    var status = new ui.Label({
-        text: 'PlayCanvas'
-    });
-    status.renderChanges = false;
+    const status = new pcui.Label();
     status.class.add('status');
     panel.append(status);
 
     // progress
-    var progress = new ui.Progress();
+    const progress = new pcui.Progress();
     progress.class.add('jobsProgress');
     panel.append(progress);
 
     // jobs
-    var jobsCount = new ui.Label({
+    const jobsCount = new pcui.Label({
         text: '0'
     });
-    jobsCount.renderChanges = false;
     jobsCount.class.add('jobsCount');
     panel.append(jobsCount);
 
@@ -32,7 +27,6 @@ editor.once('load', function () {
         status.text = text;
         status.class.remove('error');
     });
-
 
     // status error
     editor.method('status:error', function (text) {
@@ -47,21 +41,21 @@ editor.once('load', function () {
     });
 
     // update jobs
-    var updateJobs = function () {
-        var count = Object.keys(jobs).length;
+    const updateJobs = function () {
+        const count = Object.keys(jobs).length;
         jobsCount.text = count;
 
         if (count > 0) {
-            var least = 1;
+            let least = 1;
             for (const key in jobs) {
                 if (jobs[key] < least)
                     least = jobs[key];
             }
-            progress.progress = least;
+            progress.value = least * 100;
             progress.class.add('active');
         } else {
             progress.class.remove('active');
-            progress.progress = 1;
+            progress.value = 100;
         }
     };
 
