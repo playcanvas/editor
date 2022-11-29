5096772d8a5a4b79d917f495d0d59c7f15779b26
Will Eastcott <weastcott@snapchat.com>
2022-11-29 11:17:15 +0000

    Migrate code editor read only message to PCUI (#1778)
    * Migrate code editor read only message to PCUI
    
    * Tweak height
    
    Co-authored-by: Will Eastcott <will@playcanvas.com>

--- editor-ui/js/code-editor-v2/menu-panel/menu-readonly.js
+++ editor-ui/js/code-editor-v2/menu-panel/menu-readonly.js
@@ -2,15 +2,12 @@ editor.once('load', function () {
     const panel = editor.call('layout.top');
 
     // readonly label on the top right
-    const readonly = new ui.Label({
+    const readonly = new pcui.Label({
+        class: 'readonly',
+        hidden: editor.call('permissions:write'),
         text: 'READ ONLY'
     });
-
-    readonly.class.add('readonly');
-
     panel.append(readonly);
-
-    readonly.hidden = editor.call('permissions:write');
 
     editor.on('editor:readonly:change', function (isReadonly) {
         readonly.hidden = !isReadonly;
