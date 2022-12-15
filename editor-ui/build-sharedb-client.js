9f70c90719c3095c07f38494034b66c835734bb6
Iakov Sumygin <isumygin@snapchat.com>
2022-12-15 16:02:00 +0000

    support windows in sharedb client build process (#1796)

--- editor-ui/build-sharedb-client.js
+++ editor-ui/build-sharedb-client.js
@@ -1,6 +1,6 @@
-// we use browserify to compile all nodejs modules with dependencies into
-// a single share.uncompressed file to use in browser
-// ot-text is used from forked and patched https://github.com/ottypes/text
-var textType = require('ot-text').type;
-window.share = require('sharedb/lib/client');
-window.share.types.register(textType);
+var fs = require("fs");
+var browserify = require('browserify');
+var b = browserify();
+b.add('./sharedb-client.js');
+var outputFile = fs.createWriteStream('js/realtime/share.uncompressed.js');
+b.bundle().pipe(outputFile);
