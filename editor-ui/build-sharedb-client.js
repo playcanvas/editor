e11065c1e8b7c65f21abfdc9af07d4a7ea944334
Iakov Sumygin <isumygin@snapchat.com>
2022-12-20 17:32:06 +0000

    deployment fixed (#1803)

--- editor-ui/build-sharedb-client.js
+++ editor-ui/build-sharedb-client.js
@@ -2,5 +2,10 @@ var fs = require("fs");
 var browserify = require('browserify');
 var b = browserify();
 b.add('./sharedb-client.js');
+
+if (!fs.existsSync('js/realtime')) {
+    fs.mkdirSync('js/realtime');
+}
+
 var outputFile = fs.createWriteStream('js/realtime/share.uncompressed.js');
 b.bundle().pipe(outputFile);
