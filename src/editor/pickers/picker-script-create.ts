e1e0f85c8038e61ece389db3db3a01e7e208ea57
abstrakt <68276899+abstrakt8@users.noreply.github.com>
2025-09-05 11:09:06 +0100

    Validate against reserved script names when creating a script (#1424)
    * fix: check for reserved script names when creating a script
    
    * chore: disable 'no-unused-expressions' ESLint check in `test` folder

--- src/editor/pickers/picker-script-create.ts
+++ src/editor/pickers/picker-script-create.ts
@@ -1,10 +1,10 @@
+import { normalizeScriptName } from '../../common/script-names.ts';
 import { LegacyLabel } from '../../common/ui/label.ts';
 import { LegacyOverlay } from '../../common/ui/overlay.ts';
 import { LegacyTextField } from '../../common/ui/text-field.ts';
 
 editor.once('load', () => {
     let callback = null;
-    const filenameValid = /^[^0-9.#<>$+%!`&='{}@\\/:*?"|\n][^#<>$+%!`&='{}@\\/:*?"|\n]*$/;
 
     // overlay
     const overlay = new LegacyOverlay();
@@ -34,18 +34,17 @@ editor.once('load', () => {
 
         if (evt.keyCode === 13) {
             // enter
-            let filename = input.value.trim();
-            if (!filename || !filenameValid.test(filename)) {
+
+            const normalizedScriptName = normalizeScriptName(input.value);
+            const scriptNameValid = normalizedScriptName !== null;
+
+            if (!scriptNameValid) {
                 validate.hidden = false;
             } else {
                 validate.hidden = true;
 
-                if (!filename.endsWith('.js') && !filename.endsWith('.mjs')) {
-                    filename += '.js';
-                }
-
                 if (callback) {
-                    callback(filename);
+                    callback(normalizedScriptName);
                 }
 
                 overlay.hidden = true;
@@ -65,17 +64,7 @@ editor.once('load', () => {
         editor.emit('picker:script-create:close');
     });
 
-    editor.method('picker:script-create:validate', (filename) => {
-        if (!filename || !filenameValid.test(filename)) {
-            return false;
-        }
-        if (!filename.endsWith('.js') && !filename.endsWith('.mjs')) {
-            filename += '.js';
-        }
-
-        return filename;
-
-    });
+    editor.method('picker:script-create:validate', normalizeScriptName);
 
     // call picker
     editor.method('picker:script-create', (fn, string) => {
