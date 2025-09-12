f795672b8f3f30ff61aaa4e6f275cc06d0cb848a
Maksims Mihejevs <core@moka.co>
2025-09-12 18:32:18 +0300

    implements attributes clipboard conversion between different types of fields

--- src/editor/storage/clipboard-context-menu.ts
+++ src/editor/storage/clipboard-context-menu.ts
@@ -21,7 +21,6 @@ const types = new Set([
     'rgb',
     'rgba',
     'gradient',
-    'tags',
     'batchgroup',
     'layers',
     'entity',
@@ -35,6 +34,531 @@ const types = new Set([
 for (const type of assetTypes) {
     types.add(`asset:${type}`);
     types.add(`array:asset:${type}`);
+}
+
+// converts string to vector (2-4 components)
+// these formats supported:
+// 8, 16, 32
+// [ 8, 16 ]
+// { "x": 8, "z": 32 }
+const convertStringToVec = (string, valueOld) => {
+    const valueNew = [];
+    for (let i = 0; i < valueOld.length; i++) {
+        valueNew[i] = valueOld[i];
+    }
+
+    try {
+        string = string.trim();
+
+        if (!string.startsWith('[') && !string.startsWith('{')) {
+            string = `[${string}`;
+        }
+        if (!string.endsWith(']') && !string.endsWith('}')) {
+            string = `${string}]`;
+        }
+
+        const value = JSON.parse(string);
+
+        if (Array.isArray(value)) {
+            for (let i = 0; i < Math.min(value.length, valueOld.length); i++) {
+                if (typeof (value[i]) === 'number') {
+                    valueNew[i] = value[i];
+                }
+            }
+        } else if (typeof (value) === 'object') {
+            ['x', 'y', 'z', 'w'].forEach((c, i) => {
+                if (i >= valueOld.length) {
+                    return;
+                }
+                if (value.hasOwnProperty(c) && typeof (value[c]) === 'number') {
+                    valueNew[i] = value[c];
+                }
+            });
+        }
+    } catch (ex) { }
+
+    return valueNew;
+};
+
+// converts string to color (with optional alpha)
+// these formats supported:
+// #ffffffff
+// 8, 16, 32
+// [ 8, 16 ]
+// { "x": 8, "z": 32 }
+const convertStringToColor = (string, valueOld) => {
+    const valueNew = [];
+    for (let i = 0; i < valueOld.length; i++) {
+        valueNew[i] = valueOld[i];
+    }
+
+    try {
+        string = string.trim();
+
+        if (/#[0-9a-f]{6,8}/i.test(string)) {
+            const i = parseInt(string.replace('#', '0x'), 16);
+            let bytes;
+            if (string.length > 7) {
+                bytes = pc.math.intToBytes32(i);
+            } else {
+                bytes = pc.math.intToBytes24(i);
+                bytes[3] = 255;
+            }
+
+            valueNew[0] = bytes[0] / 255;
+            valueNew[1] = bytes[1] / 255;
+            valueNew[2] = bytes[2] / 255;
+            if (valueOld.length === 4) {
+                valueNew[3] = bytes[3] / 255;
+            }
+        } else {
+            if (!string.startsWith('[') && !string.startsWith('{')) {
+                string = `[${string}`;
+            }
+            if (!string.endsWith(']') && !string.endsWith('}')) {
+                string = `${string}]`;
+            }
+
+            const value = JSON.parse(string);
+
+            if (Array.isArray(value)) {
+                for (let i = 0; i < Math.min(value.length, valueOld.length); i++) {
+                    if (typeof (value[i]) === 'number') {
+                        valueNew[i] = value[i];
+                    }
+                }
+            } else if (typeof (value) === 'object') {
+                ['r', 'g', 'b', 'a'].forEach((c, i) => {
+                    if (i >= valueOld.length) {
+                        return;
+                    }
+                    if (value.hasOwnProperty(c) && typeof (value[c]) === 'number') {
+                        valueNew[i] = value[c];
+                    }
+                });
+            }
+        }
+    } catch (ex) { }
+
+    return valueNew;
+};
+
+// list of conversion methods,
+// it uses new value (n) and optionally an old value (o)
+const convertTypes = new Map([
+    [
+        'boolean-string',
+        (n, o) => {
+            return n ? 'true' : 'false';
+        }
+    ], [
+        'boolean-text',
+        (n, o) => {
+            return n ? 'true' : 'false';
+        }
+    ], [
+        'boolean-number',
+        (n, o) => {
+            return n ? 1 : 0;
+        }
+    ], [
+        'boolean-slider',
+        (n, o) => {
+            return n ? 1 : 0;
+        }
+    ], [
+        'string-text',
+        (n, o) => {
+            return n;
+        }
+    ], [
+        'string-tags',
+        (n, o) => {
+            const set = new Set();
+            const items = n.split(',');
+            for (let i = 0; i < items.length; i++) {
+                const tag = items[i].trim();
+                if (!tag) {
+                    continue;
+                }
+                set.add(tag);
+            }
+            if (set.size === 0) {
+                return [];
+            }
+            return Array.from(set);
+        }
+    ], [
+        'string-number',
+        (n, o) => {
+            const number = parseFloat(n);
+            if (isNaN(number)) {
+                return 0;
+            }
+            return number;
+        }
+    ], [
+        'string-slider',
+        (n, o) => {
+            const number = parseFloat(n);
+            if (isNaN(number)) {
+                return 0;
+            }
+            return number;
+        }
+    ], [
+        'string-vec2',
+        convertStringToVec
+    ], [
+        'string-vec3',
+        convertStringToVec
+    ], [
+        'string-vec4',
+        convertStringToVec
+    ], [
+        'string-rgb',
+        convertStringToColor
+    ], [
+        'string-rgba',
+        convertStringToColor
+    ], [
+        'array:string-string',
+        (n, o) => {
+            return n.join(', ');
+        }
+    ], [
+        'array:string-text',
+        (n, o) => {
+            return n.join('\n');
+        }
+    ], [
+        'array:string-tags',
+        (n, o) => {
+            const set = new Set();
+            for (let i = 0; i < n.length; i++) {
+                if (!n[i]) {
+                    continue;
+                }
+                set.add(n[i]);
+            }
+            if (set.size === 0) {
+                return [];
+            }
+            return Array.from(set);
+        }
+    ], [
+        'text-string',
+        (n, o) => {
+            return n;
+        }
+    ], [
+        'text-array:string',
+        (n, o) => {
+            const items = n.split('\n');
+            if (items.length === 1 && items[0] === '') {
+                return [];
+            }
+            return items;
+        }
+    ], [
+        'text-tags',
+        (n, o) => {
+            const set = new Set();
+            const items = n.split('\n');
+            for (let i = 0; i < items.length; i++) {
+                const tag = items[i].trim();
+                if (!tag) {
+                    continue;
+                }
+                set.add(tag);
+            }
+            if (set.size === 0) {
+                return [];
+            }
+            return Array.from(set);
+        }
+    ], [
+        'tags-string',
+        (n, o) => {
+            return n.join(', ');
+        }
+    ], [
+        'tags-array:string',
+        (n, o) => {
+            return n;
+        }
+    ], [
+        'tags-text',
+        (n, o) => {
+            return n.join('\n');
+        }
+    ], [
+        'number-boolean',
+        (n, o) => {
+            return !!n;
+        }
+    ], [
+        'number-string',
+        (n, o) => {
+            return `${n}`;
+        }
+    ], [
+        'number-text',
+        (n, o) => {
+            return `${n}`;
+        }
+    ], [
+        'number-slider',
+        (n, o) => {
+            return n;
+        }
+    ], [
+        'number-vec2',
+        (n, o) => {
+            return [n, o[1]];
+        }
+    ], [
+        'number-vec3',
+        (n, o) => {
+            return [n, o[1], o[2]];
+        }
+    ], [
+        'number-vec4',
+        (n, o) => {
+            return [n, o[1], o[2], o[3]];
+        }
+    ], [
+        'number-rgb',
+        (n, o) => {
+            return [n, o[1], o[2]];
+        }
+    ], [
+        'number-rgba',
+        (n, o) => {
+            return [n, o[1], o[2], o[3]];
+        }
+    ], [
+        'number-asset',
+        (n, o) => {
+            if (!n) {
+                return o;
+            }
+            const asset = editor.call('assets:get', n);
+            if (!asset) {
+                return o;
+            }
+            return n;
+        }
+    ], [
+        'vec2-string',
+        (n, o) => {
+            return JSON.stringify(n);
+        }
+    ], [
+        'vec2-text',
+        (n, o) => {
+            return JSON.stringify(n);
+        }
+    ], [
+        'vec2-vec3',
+        (n, o) => {
+            return [n[0], n[1], o[2]];
+        }
+    ], [
+        'vec2-vec4',
+        (n, o) => {
+            return [n[0], n[1], o[2], o[3]];
+        }
+    ], [
+        'vec2-rgb',
+        (n, o) => {
+            return [n[0], n[1], o[2]];
+        }
+    ], [
+        'vec2-rgba',
+        (n, o) => {
+            return [n[0], n[1], o[2], o[3]];
+        }
+    ], [
+        'vec3-string',
+        (n, o) => {
+            return JSON.stringify(n);
+        }
+    ], [
+        'vec3-text',
+        (n, o) => {
+            return JSON.stringify(n);
+        }
+    ], [
+        'vec3-vec2',
+        (n, o) => {
+            return [n[0], n[1]];
+        }
+    ], [
+        'vec3-vec4',
+        (n, o) => {
+            return [n[0], n[1], n[2], o[3]];
+        }
+    ], [
+        'vec3-rgb',
+        (n, o) => {
+            return [n[0], n[1], n[2]];
+        }
+    ], [
+        'vec3-rgba',
+        (n, o) => {
+            return [n[0], n[1], n[2], o[3]];
+        }
+    ], [
+        'vec4-string',
+        (n, o) => {
+            return JSON.stringify(n);
+        }
+    ], [
+        'vec4-text',
+        (n, o) => {
+            return JSON.stringify(n);
+        }
+    ], [
+        'vec4-vec2',
+        (n, o) => {
+            return [n[0], n[1]];
+        }
+    ], [
+        'vec4-vec3',
+        (n, o) => {
+            return [n[0], n[1], n[2]];
+        }
+    ], [
+        'vec4-rgb',
+        (n, o) => {
+            return [n[0], n[1], n[2]];
+        }
+    ], [
+        'vec4-rgba',
+        (n, o) => {
+            return [n[0], n[1], n[2], n[3]];
+        }
+    ], [
+        'rgb-string',
+        (n, o) => {
+            return pc.Color.prototype.toString.call({
+                r: n[0],
+                g: n[1],
+                b: n[2],
+                a: 0
+            }, false).toUpperCase();
+        }
+    ], [
+        'rgb-text',
+        (n, o) => {
+            return pc.Color.prototype.toString.call({
+                r: n[0],
+                g: n[1],
+                b: n[2],
+                a: 0
+            }, false).toUpperCase();
+        }
+    ], [
+        'rgb-vec2',
+        (n, o) => {
+            return [n[0], n[1]];
+        }
+    ], [
+        'rgb-vec3',
+        (n, o) => {
+            return [n[0], n[1], n[2]];
+        }
+    ], [
+        'rgb-vec4',
+        (n, o) => {
+            return [n[0], n[1], n[2], o[3]];
+        }
+    ], [
+        'rgb-rgba',
+        (n, o) => {
+            return [n[0], n[1], n[2], o[3]];
+        }
+    ], [
+        'rgba-string',
+        (n, o) => {
+            return pc.Color.prototype.toString.call({
+                r: n[0],
+                g: n[1],
+                b: n[2],
+                a: n[3]
+            }, true).toUpperCase();
+        }
+    ], [
+        'rgba-text',
+        (n, o) => {
+            return pc.Color.prototype.toString.call({
+                r: n[0],
+                g: n[1],
+                b: n[2],
+                a: n[3]
+            }, true).toUpperCase();
+        }
+    ], [
+        'rgba-vec2',
+        (n, o) => {
+            return [n[0], n[1]];
+        }
+    ], [
+        'rgba-vec3',
+        (n, o) => {
+            return [n[0], n[1], n[2]];
+        }
+    ], [
+        'rgba-vec4',
+        (n, o) => {
+            return [n[0], n[1], n[2], n[3]];
+        }
+    ], [
+        'rgba-rgb',
+        (n, o) => {
+            return [n[0], n[1], n[2]];
+        }
+    ]
+]);
+
+
+// additional assets conversions
+for (const type of assetTypes) {
+    convertTypes.set(`asset:${type}-asset`, (n, o) => {
+        return n;
+    });
+
+    convertTypes.set(`asset-asset:${type}`, (n, o) => {
+        if (!n) {
+            return null;
+        }
+
+        const assetType = editor.call('assets:get', n)?.get('type');
+        if (!assetType) {
+            return o;
+        }
+
+        if (assetType === type) {
+            return n;
+        }
+
+        return o;
+    });
+
+    convertTypes.set(`number-asset:${type}`, (n, o) => {
+        if (!n) {
+            return o;
+        }
+        const asset = editor.call('assets:get', n);
+        if (!asset) {
+            return o;
+        }
+        if (asset.get('type') !== type) {
+            return o;
+        }
+        return n;
+    });
 }
 
 
@@ -84,14 +608,6 @@ editor.once('load', () => {
             value.hasOwnProperty('value') &&
             value.type;
     };
-
-
-    // TODO:
-    // type to other type conversions, e.g.:
-    // asset:* > asset
-    // asset > asset:* - if copied asset is of desired type
-    // rgb <> rgba
-
 
     // context menu
     const menu = new Menu({
@@ -230,8 +746,33 @@ editor.once('load', () => {
         }
 
         // types should match
+        // or there should be a valid conversion option
         if (paste.type !== type) {
-            return false;
+            if (!convertTypes.has(`${paste.type}-${type}`)) {
+                return false;
+            } else if (paste.type === 'asset' && type.startsWith('asset:') && paste.value) {
+                const assetType = editor.call('assets:get', paste.value)?.get('type');
+                if (!assetType) {
+                    return false;
+                }
+                if (`asset:${assetType}` !== type) {
+                    return false;
+                }
+            } else if (paste.type === 'number' && type === 'asset' && paste.value) {
+                const asset = editor.call('assets:get', paste.value);
+                if (!asset) {
+                    return false;
+                }
+            } else if (paste.type === 'number' && type.startsWith('asset:') && paste.value) {
+                const asset = editor.call('assets:get', paste.value);
+                if (!asset) {
+                    return false;
+                }
+                const assetType = asset.get('type');
+                if (`asset:${assetType}` !== type) {
+                    return false;
+                }
+            }
         }
 
         // if options are provided
@@ -336,6 +877,12 @@ editor.once('load', () => {
 
         const paste = clipboard.value;
 
+        const convert = paste.type !== type;
+        const conversionTuple = `${paste.type}-${type}`;
+        if (convert && !convertTypes.has(conversionTuple)) {
+            return false;
+        }
+
         // TODO:
         // verify if value is actually valid based on type
 
@@ -343,17 +890,20 @@ editor.once('load', () => {
         const records = [];
 
         for (let i = 0; i < items.length; i++) {
+            const valueOld = items[i].get(path);
+            const valueNew = convert ? convertTypes.get(conversionTuple)(paste.value, valueOld) : paste.value;
+
             // create history records
             records.push({
                 item: items[i],
                 path: path,
-                valueOld: items[i].get(path),
-                valueNew: paste.value
+                valueOld: valueOld,
+                valueNew: valueNew
             });
 
             // paste new value
             items[i].history.enabled = false;
-            items[i].set(path, paste.value);
+            items[i].set(path, valueNew);
             items[i].history.enabled = true;
 
             // TODO:
