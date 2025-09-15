5540303e6bf923fee1f4440353e3db406bcba56f
abstrakt <abstrakt314@gmail.com>
2025-09-15 14:15:55 +0100

    feat(templates): implement "Unlink From Template" for multi-selection

--- src/editor/templates/unlink-template.ts
+++ src/editor/templates/unlink-template.ts
@@ -1,64 +1,89 @@
+// TODO: Better types once available
+type Entity = any;
+type SnapshotEntry = {
+    entityId: any;
+    templateId: any;
+    template_ent_ids: any;
+};
+
 editor.once('load', () => {
-    editor.method('templates:unlink', (entity) => {
+    function extractEntityIds(entities: Entity | Entity[]): string[] {
+        if (!Array.isArray(entities)) {
+            entities = [entities];
+        }
+        return entities.filter(e => e.get('template_id')).map(e => e.get('resource_id'));
+    }
+
+    editor.method('templates:unlink', (entities: Entity | Entity[]) => {
         if (!editor.call('permissions:write')) {
             return;
         }
 
-        let templateId, template_ent_ids;
+        const entityIds = extractEntityIds(entities);
+        let snapshots: SnapshotEntry[] = [];
 
         function undo() {
-            entity = entity.latest();
-            if (!entity) {
-                return;
-            }
-
-            const history = entity.history.enabled;
-            entity.history.enabled = false;
-
-            // if template asset does not exist anymore then skip undo
-            const asset = editor.call('assets:get', templateId);
-            if (!asset) {
-                return;
-            }
-
-            // remove invalid entries from template_ent_ids
-            for (const id in template_ent_ids) {
-                if (!asset.has(`data.entities.${template_ent_ids[id]}`)) {
-                    delete template_ent_ids[id];
+            snapshots.forEach(({ entityId, template_ent_ids, templateId }) => {
+                const entity = editor.call('entities:get', entityId);
+                if (!entity) {
+                    return;
                 }
-            }
 
-            entity.set('template_id', templateId);
-            entity.set('template_ent_ids', template_ent_ids);
+                const asset = editor.call('assets:get', templateId);
+                if (!asset) {
+                    return;
+                }
 
-            entity.history.enabled = history;
+                // remove invalid entries from template_ent_ids
+                for (const id in template_ent_ids) {
+                    if (!asset.has(`data.entities.${template_ent_ids[id]}`)) {
+                        delete template_ent_ids[id];
+                    }
+                }
+
+                const history = entity.history.enabled;
+                entity.history.enabled = false;
+                entity.set('template_id', templateId);
+                entity.set('template_ent_ids', template_ent_ids);
+                entity.history.enabled = history;
+            });
         }
 
         function redo() {
-            entity = entity.latest();
-            if (!entity) {
-                return;
-            }
+            snapshots = [];
 
-            templateId = entity.get('template_id');
-            template_ent_ids = entity.get('template_ent_ids');
+            entityIds.forEach((entityId) => {
+                const entity = editor.call('entities:get', entityId);
+                if (!entity) {
+                    return;
+                }
 
-            if (!templateId) {
-                return;
-            }
+                const templateId = entity.get('template_id');
+                const template_ent_ids = entity.get('template_ent_ids');
 
-            const history = entity.history.enabled;
-            entity.history.enabled = false;
-            entity.unset('template_id');
-            entity.unset('template_ent_ids');
-            entity.history.enabled = history;
+                if (!templateId) {
+                    return;
+                }
+
+                snapshots.push({
+                    entityId,
+                    templateId,
+                    template_ent_ids
+                });
+
+                const history = entity.history.enabled;
+                entity.history.enabled = false;
+                entity.unset('template_id');
+                entity.unset('template_ent_ids');
+                entity.history.enabled = history;
+            });
         }
 
         editor.api.globals.history.add({
             name: 'unlink template',
             combine: false,
-            undo: undo,
-            redo: redo
+            undo,
+            redo
         });
 
         redo();
