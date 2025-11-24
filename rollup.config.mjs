038eec3b68c2107e37d7467f41f166293e3b283f
kpal <kpal81xd@gmail.com>
2025-11-24 13:57:52 +0000

    feat: added editor-api build to base editor

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,8 +6,11 @@ import resolve from '@rollup/plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 import swc from '@rollup/plugin-swc';
 import copy from 'rollup-plugin-copy';
+import { dts } from 'rollup-plugin-dts';
 import polyfills from 'rollup-plugin-polyfill-node';
 import tsconfigPaths from 'rollup-plugin-tsconfig-paths';
+
+import { runTsc } from './utils/plugins/rollup-run-tsc.mjs';
 
 const production = process.env.NODE_ENV === 'production';
 const sourcemap = production ? true : 'inline';
@@ -47,6 +50,59 @@ const plugins = () => {
         })
     ];
 };
+
+const footer = `export as namespace api;
+declare global {
+    const editor: typeof globals;
+}`;
+
+const API_TARGETS = [
+    {
+        input: 'modules/editor-api/src/index.ts',
+        output: {
+            file: 'modules/editor-api/dist/index.js',
+            format: 'esm',
+            globals: {
+                '@playcanvas/observer': 'observer'
+            }
+        },
+        external: ['@playcanvas/observer'],
+        plugins: [
+            commonjs(),
+            polyfills(),
+            resolve({
+                extensions: ['.ts', '.js', '.json'],
+                browser: true
+            }),
+            swc({
+                swc: {
+                    jsc: {
+                        externalHelpers: false
+                    },
+                    env: {
+                        loose: true,
+                        bugfixes: true,
+                        targets: {
+                            chrome: 63 // supports esmodules
+                        }
+                    }
+                }
+            })
+        ]
+    },
+    {
+        input: 'modules/editor-api/types/index.d.ts',
+        output: [{
+            file: 'modules/editor-api/dist/index.d.ts',
+            footer: footer,
+            format: 'es'
+        }],
+        plugins: [
+            runTsc('modules/editor-api/tsconfig.build.json'),
+            dts()
+        ]
+    }
+];
 
 const PAGE_TARGETS = [
     {
@@ -275,6 +331,7 @@ const SERVICE_WORKER_TARGETS = fs.readdirSync('src/sw').map((file) => {
 });
 
 export default [
+    ...API_TARGETS,
     ...PAGE_TARGETS,
     ...PLUGIN_TARGETS,
     ...WORKER_TARGETS,
