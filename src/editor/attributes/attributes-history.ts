0db894cca6360770defe42758e3a9ec3093bec64
abstrakt <68276899+abstrakt8@users.noreply.github.com>
2025-12-03 16:17:22 +0000

    Use KeyboardEvent#code in camera-fly.ts (#1460)
    * refactor: Use KeyboardEvent#code in camera-fly.ts
    
    * refactor: shift+x instead of shift+z for previous selection to avoid conflict with AZERTY orbital movement

--- src/editor/attributes/attributes-history.ts
+++ src/editor/attributes/attributes-history.ts
@@ -135,7 +135,7 @@ editor.once('load', () => {
     });
 
     editor.call('hotkey:register', 'selector:return', {
-        key: 'z',
+        key: 'x',
         shift: true,
         callback: () => {
             if (editor.call('picker:isOpen:otherThan', 'curve')) {
