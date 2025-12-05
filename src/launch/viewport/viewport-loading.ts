dacf7e5f82c4a67b632d6af1f74dd053dd36248c
Will Eastcott <will@playcanvas.com>
2025-12-05 17:49:22 +0000

    Modernize client-side loading screen code (#1592)

--- src/launch/viewport/viewport-loading.ts
+++ src/launch/viewport/viewport-loading.ts
@@ -1,13 +1,63 @@
 editor.once('load', () => {
     editor.method('viewport:loadingScreen', () => {
         pc.script.createLoadingScreen((app) => {
-            const showSplash = function () {
-                // splash wrapper
+            const createCss = () => {
+                const css = `
+                    body {
+                        background-color: #283538;
+                    }
+
+                    #application-splash-wrapper {
+                        position: absolute;
+                        top: 0;
+                        left: 0;
+                        height: 100%;
+                        width: 100%;
+                        background-color: #283538;
+                    }
+
+                    #application-splash {
+                        position: absolute;
+                        top: calc(50% - 28px);
+                        width: 264px;
+                        left: calc(50% - 132px);
+                    }
+
+                    #application-splash img {
+                        width: 100%;
+                    }
+
+                    #progress-bar-container {
+                        margin: 20px auto 0 auto;
+                        height: 2px;
+                        width: 100%;
+                        background-color: #1d292c;
+                    }
+
+                    #progress-bar {
+                        width: 0%;
+                        height: 100%;
+                        background-color: #f60;
+                    }
+
+                    @media (max-width: 480px) {
+                        #application-splash {
+                            width: 170px;
+                            left: calc(50% - 85px);
+                        }
+                    }
+                `;
+
+                const style = document.createElement('style');
+                style.textContent = css;
+                document.head.appendChild(style);
+            };
+
+            const showSplash = () => {
                 const wrapper = document.createElement('div');
                 wrapper.id = 'application-splash-wrapper';
                 document.body.appendChild(wrapper);
 
-                // splash
                 const splash = document.createElement('div');
                 splash.id = 'application-splash';
                 wrapper.appendChild(splash);
@@ -16,7 +66,7 @@ editor.once('load', () => {
                 const logo = document.createElement('img');
                 logo.src = 'https://playcanvas.com/static-assets/images/play_text_252_white.png';
                 splash.appendChild(logo);
-                logo.onload = function () {
+                logo.onload = () => {
                     splash.style.display = 'block';
                 };
 
@@ -27,15 +77,9 @@ editor.once('load', () => {
                 const bar = document.createElement('div');
                 bar.id = 'progress-bar';
                 container.appendChild(bar);
-
             };
 
-            const hideSplash = function () {
-                const splash = document.getElementById('application-splash-wrapper');
-                splash.parentElement.removeChild(splash);
-            };
-
-            const setProgress = function (value) {
+            const setProgress = (value) => {
                 const bar = document.getElementById('progress-bar');
                 if (bar) {
                     value = Math.min(1, Math.max(0, value));
@@ -43,67 +87,11 @@ editor.once('load', () => {
                 }
             };
 
-            const createCss = function () {
-                const css = [
-                    'body {',
-                    '    background-color: #283538;',
-                    '}',
-
-                    '#application-splash-wrapper {',
-                    '    position: absolute;',
-                    '    top: 0;',
-                    '    left: 0;',
-                    '    height: 100%;',
-                    '    width: 100%;',
-                    '    background-color: #283538;',
-                    '}',
-
-                    '#application-splash {',
-                    '    position: absolute;',
-                    '    top: calc(50% - 28px);',
-                    '    width: 264px;',
-                    '    left: calc(50% - 132px);',
-                    '}',
-
-                    '#application-splash img {',
-                    '    width: 100%;',
-                    '}',
-
-                    '#progress-bar-container {',
-                    '    margin: 20px auto 0 auto;',
-                    '    height: 2px;',
-                    '    width: 100%;',
-                    '    background-color: #1d292c;',
-                    '}',
-
-                    '#progress-bar {',
-                    '    width: 0%;',
-                    '    height: 100%;',
-                    '    background-color: #f60;',
-                    '}',
-                    '@media (max-width: 480px) {',
-                    '    #application-splash {',
-                    '        width: 170px;',
-                    '        left: calc(50% - 85px);',
-                    '    }',
-                    '}'
-
-                ].join('\n');
-
-                const style = document.createElement('style');
-                style.type = 'text/css';
-                if (style.styleSheet) {
-                    style.styleSheet.cssText = css;
-                } else {
-                    style.appendChild(document.createTextNode(css));
-                }
-
-                document.head.appendChild(style);
+            const hideSplash = () => {
+                document.getElementById('application-splash-wrapper').remove();
             };
 
-
             createCss();
-
             showSplash();
 
             app.on('preload:end', () => {
