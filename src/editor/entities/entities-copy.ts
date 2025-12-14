21c866c571f6118bec5223347549b554eafb52c7
Will Eastcott <will@playcanvas.com>
2025-12-14 11:18:13 +0000

    Tweak error check

--- src/editor/entities/entities-copy.ts
+++ src/editor/entities/entities-copy.ts
@@ -8,8 +8,7 @@ editor.once('load', () => {
         try {
             editor.api.globals.entities.copyToClipboard(entities.map(e => e.apiEntity));
         } catch (err) {
-            if (err.name === 'QuotaExceededError' ||
-                (err instanceof DOMException && err.code === 22)) {
+            if (err.name === 'QuotaExceededError') {
                 editor.call('status:error', 'Cannot copy: Selection is too large');
             } else {
                 editor.call('status:error', `Copy failed: ${err.message}`);
