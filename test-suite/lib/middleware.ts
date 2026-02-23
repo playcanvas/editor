1252621ebaa02b0d432da5f8e9271cdd066baa1f
kpal <kpal81xd@gmail.com>
2026-02-23 16:31:05 +0000

    fix: removed rate limits

--- test-suite/lib/middleware.ts
+++ test-suite/lib/middleware.ts
@@ -1,64 +1,6 @@
 import { type BrowserContext } from '@playwright/test';
-import Bottleneck from 'bottleneck';
 
 import { HEADER_NAME, HEADER_VALUE } from './config';
-
-const MAX_CONCURRENT = 10;
-const ONE_MINUTE = 60 * 1000;
-const RPM = {
-    api: {
-        normal: 120,
-        strict: 5,
-        assets: 120,
-        post: 30
-    }
-};
-
-const limiters = {
-    api: {
-        normal: new Bottleneck({
-            maxConcurrent: MAX_CONCURRENT,
-            reservoir: RPM.api.normal,
-            reservoirRefreshAmount: RPM.api.normal,
-            reservoirRefreshInterval: ONE_MINUTE
-        }),
-        strict: new Bottleneck({
-            maxConcurrent: MAX_CONCURRENT,
-            reservoir: RPM.api.strict,
-            reservoirRefreshAmount: RPM.api.strict,
-            reservoirRefreshInterval: ONE_MINUTE
-        }),
-        assets: new Bottleneck({
-            maxConcurrent: MAX_CONCURRENT,
-            reservoir: RPM.api.assets,
-            reservoirRefreshAmount: RPM.api.assets,
-            reservoirRefreshInterval: ONE_MINUTE
-        }),
-        post: new Bottleneck({
-            maxConcurrent: MAX_CONCURRENT,
-            reservoir: RPM.api.post,
-            reservoirRefreshAmount: RPM.api.post,
-            reservoirRefreshInterval: ONE_MINUTE
-        })
-    }
-};
-
-const STRICT_ROUTES = [
-    /api\/\d+\/download$/,
-    /api\/projects\/upload$/,
-    /api\/upload\/signed-url$/,
-    /api\/upload\/start-upload$/,
-    /api\/apps$/,
-    /api\/apps\/download$/,
-    /api\/splats$/,
-    /api\/splats\/publish$/
-];
-
-const ASSET_ROUTES = [
-    /api$/,
-    /api\/\d+$/,
-    /api\/\d+\/reimport$/
-];
 
 export const middleware = async (context: BrowserContext) => {
     // mock OneTrust script to prevent cookie popups interfering with tests
@@ -78,25 +20,5 @@ export const middleware = async (context: BrowserContext) => {
                 [HEADER_NAME]: HEADER_VALUE
             }
         });
-    });
-
-    // rate limit API requests
-    await context.route(/playcanvas\.com\/api/, (route, request) => {
-        const url = new URL(request.url());
-        const method = request.method();
-
-        if (STRICT_ROUTES.some(re => re.test(url.pathname))) {
-            return limiters.api.strict.schedule(() => route.continue());
-        }
-
-        if (ASSET_ROUTES.some(re => re.test(url.pathname))) {
-            return limiters.api.assets.schedule(() => route.continue());
-        }
-
-        if (method === 'POST') {
-            return limiters.api.post.schedule(() => route.continue());
-        }
-
-        return limiters.api.normal.schedule(() => route.continue());
     });
 };
