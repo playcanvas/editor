950358d0434eaae803a188091ba371d5d5c463cc
KPal <48248865+kpal81xd@users.noreply.github.com>
2026-02-24 10:13:57 +0000

    [Test Suite] Removed rate limits (#1784)
    * fix: removed rate limits
    
    * fix: update authentication check in test setup

--- test-suite/test/auth.setup.ts
+++ test-suite/test/auth.setup.ts
@@ -3,13 +3,22 @@ import { test as setup } from '@playwright/test';
 import { HOST } from '../lib/config';
 import { middleware } from '../lib/middleware';
 
-setup('authenticate user', async ({ page }) => {
+setup('user authenticated', async ({ page }) => {
     await middleware(page.context());
+
+    // check if already authenticated by looking for editor title
     await page.goto(`https://${HOST}/editor`, { waitUntil: 'networkidle' });
     const title = await page.title();
-    if (/Editor/.test(title)) {
-        setup.skip(true, 'already authenticated');
-        return;
+    if (!/Editor/.test(title)) {
+        throw new Error('not authenticated');
     }
-    throw new Error('not authenticated');
+
+    // check for test suite flag present on account
+    const res1 = await page.request.get(`https://${HOST}/api/id`);
+    const { id } = await res1.json();
+    const res2 = await page.request.get(`https://${HOST}/api/users/${id}`);
+    const { flags } = await res2.json();
+    if (!flags?.testSuite) {
+        throw new Error('test suite flag not present on account');
+    }
 });
