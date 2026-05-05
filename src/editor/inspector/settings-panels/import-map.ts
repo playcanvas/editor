1f58c434608bad44c3cbdfc62717ce6caea3e524
kpal <kpal81xd@gmail.com>
2026-05-05 12:37:36 +0100

    fix: create default import map as json

--- src/editor/inspector/settings-panels/import-map.ts
+++ src/editor/inspector/settings-panels/import-map.ts
@@ -144,7 +144,7 @@ class ImportMapSettingsPanel extends BaseSettingsPanel {
     _clickCreateDefault() {
         this._createDefaultButton.enabled = false;
         editor.call('assets:create:json', {
-            name: 'Import Map',
+            name: 'import-map.json',
             json: JSON.stringify({ imports: {} }, null, 2),
             spaces: 2,
             noSelect: true,
